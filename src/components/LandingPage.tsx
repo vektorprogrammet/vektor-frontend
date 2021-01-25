2c827af4ec1225a40e95a786bcd3552f9b8e0edc
lundli <lundli@users.noreply.github.com>
2021-01-25 19:36:16 +0100

    Remove default landing page

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const LandingPage: React.FC = () => {
+    
+    return ( 
+        <div>
+            <h1>Vektorprogrammet</h1>
+            <br></br>
+            <p>Frontend</p>
+            <br></br>
+        </div>
+    )
+}
+
+export default LandingPage;
