00e3b8a70976607b61352b41363bf7aa5f994d44
Vilde <vildethorstensen@Vildes-MacBook-Pro-2.local>
2021-02-16 19:29:36 +0100

    Changed back to AppHeader

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AppHeader from './components/Header/Header'
+import AppHeader from './components/Header/AppHeader'
 
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
