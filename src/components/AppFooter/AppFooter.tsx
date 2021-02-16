c40a3c38262d6c0c58c746b79b9c6aa944edf596
malin <malindoskeland@gmail.com>
2021-02-16 18:55:48 +0100

    Added Footer component

---
+++ src/components/AppFooter/AppFooter.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AppFooter: React.FC = () => {
+    
+    return (
+        <div style={{backgroundColor: '#023874', position: "absolute", bottom: "0",
+         width: "100%", height: "3.5rem"}}>
+            <h1>
+                Footer
+            </h1>
+        </div>
+    )   
+}
+
+export default AppFooter;
