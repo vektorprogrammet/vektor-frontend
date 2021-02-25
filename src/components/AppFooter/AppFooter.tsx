c9c691704e8709ea0fa36c6ae18b261f22ad07bf
Hans Tjøtta <hahotjo@gmail.com>
2021-02-25 17:40:30 +0100

    Added sponsor-list to footer

--- src/components/AppFooter/AppFooter.tsx
+++ src/components/AppFooter/AppFooter.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from 'react';
+import FooterSponsors from './FooterSponsors';
 
-const AppFooter = () => {
+const AppFooter = (): JSX.Element => {
     
     return (
         <div style={{backgroundColor: '#023874', position: "absolute", bottom: "0",
-         width: "100%", height: "3.5rem"}}>
+         width: "100%"}}>
             <h1>
                 Footer
             </h1>
+            <p>Sponsorer og samarbeidspartnere:</p>
+            <FooterSponsors/>
         </div>
-    )   
-}
+    );
+};
 
 export default AppFooter;
---
+++ src/components/AppFooter/FooterSponsors.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import getSponsors, {Sponsor} from '../../api/Sponsor';
+
+
+
+const FooterSponsors = (): JSX.Element => {
+    const sponsors: Array<Sponsor> = getSponsors();
+
+    const sponsorItems: Array<JSX.Element> = sponsors.map((sponsor: Sponsor, index: number) => 
+        <li key={index} ><a style={{color: "white"}} href={sponsor.url.href}>{sponsor.name}</a> </li>
+    );
+
+    return (
+        <ul>
+            { sponsorItems }
+        </ul>
+    );
+};
+
+export default FooterSponsors;
