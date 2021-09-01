c59606fa595446003b853fe3d6ff507615340eb4
haved <krogstie.havard@gmail.com>
2021-09-02 01:36:51 +0200

    [VF-18] fix: give new alt text to footer logo to fix test

--- src/components/AppFooter/AppFooter.tsx
+++ src/components/AppFooter/AppFooter.tsx
@@ -7,7 +7,7 @@ const AppFooter = (): JSX.Element => {
   return (
     <footer className="Footer">
       <div className="max-w-6xl mx-auto p-2 py-8 flex justify-between place-items-center flex-col space-y-8 lg:flex-row lg:space-x-4 lg:space-y-0">
-        <img src="images/vektor-logo-white.svg" alt="vektorprogrammet logo" className="h-24 md:h-40" />
+        <img src="images/vektor-logo-white.svg" alt="vektorprogrammet logo hvit" className="h-24 md:h-40" />
         <FooterLinks />
         <FooterSponsors />
         <DarkModeToggle />
