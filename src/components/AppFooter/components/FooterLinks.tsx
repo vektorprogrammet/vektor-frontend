9cf98723087c396a5ec3bf84f7f22f5055465982
haved <krogstie.havard@gmail.com>
2021-09-07 00:00:13 +0200

    fix(footer): use grid to align icons and text in footer

--- src/components/AppFooter/components/FooterLinks.tsx
+++ src/components/AppFooter/components/FooterLinks.tsx
@@ -3,24 +3,26 @@ import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import "./FooterLinks.css";
+
 const FooterLinks = () => {
   return (
-    <div className="text-white space-y-4">
+    <div className="text-white footerLinks">
+      <FontAwesomeIcon icon={faFacebookSquare} size="2x" className="icon" />
       <div className="flex place-items-center space-x-2">
-        <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
         <a href="https://www.facebook.com/vektorprogrammet/">Trondheim</a>
         <a href="https://www.facebook.com/vektorprogrammetNMBU/">Ås</a>
         <a href="https://www.facebook.com/Vektorprogrammet-UiO-1017596031601586/">Oslo</a>
         <a href="https://www.facebook.com/VektorprogrammetBergen/">Bergen</a>
       </div>
 
+      <FontAwesomeIcon icon={faEnvelope} size="2x" className="icon" />
       <div className="flex place-items-center space-x-2">
-        <FontAwesomeIcon icon={faEnvelope} size="2x" />
         <a href="mailto:hovedstyret@vektorprogrammet.no">hovedstyret@vektorprogrammet.no</a>
       </div>
 
+      <FontAwesomeIcon icon={faMapMarkerAlt} size="2x" className="icon" />
       <div className="flex place-items-center space-x-2">
-        <FontAwesomeIcon icon={faMapMarkerAlt} size="2x" />
         <div>
           Høgskoleringen 5, 7491 Trondheim
         </div>
