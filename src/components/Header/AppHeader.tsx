956fbbc2dd357a63df253ed07f52ce933c92b78a
Erlend Marius Ommundsen <emommundsen@gmail.com>
2021-10-04 19:55:23 +0200

    [VF-41] Modify Appheader for mobile menu

--- src/components/Header/AppHeader.tsx
+++ src/components/Header/AppHeader.tsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import routes from "../../pages/public/routes";
+import MobileMenu from "./MobileMenu";
 
 const AppHeader = (): JSX.Element => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const linkElements = routes.map(
+    (route) => (
+      <NavLink
+        key={route.route}
+        className="mt-4 vektor-font font-semibold p-1"
+        to={route.route}
+      >
+        {route.component.name.replace(/([A-Z])/g, " $1").trim()}
+      </NavLink>
+    ),
+  );
+
   return (
-    <nav className="AppHeader shadow-md">
-      <div className="max-w-6xl mx-auto p-2 flex space-x-8 items-end">
-        <Link to="/">
-          <img src="images/vektor-logo.svg" alt="vektorprogrammet logo" className="h-20" />
-        </Link>
-        <div className="flex-grow" />
-        <Link className="header-link" to="/assistenter">Assistenter</Link>
-        <Link className="header-link" to="/om-vektor">Om oss</Link>
-        <Link className="header-link" to="/skoler">For skoler</Link>
-        <Link className="header-link" to="/foreldre">For foreldre</Link>
-        <Link className="header-link" to="/team">Team</Link>
-        <Link className="header-link" to="/kontakt">Kontakt</Link>
-      </div>
-    </nav>
+    <>
+      <nav className="AppHeader shadow-md">
+        <div className="max-w-8xl hidden md:flex  mx-auto p-2 space-x-6 items-end">
+          <Link to="/">
+            <img src="images/vektor-logo.svg" alt="vektorprogrammet logo" className="h-20" />
+          </Link>
+          <div className="flex-grow" />
+          {linkElements}
+        </div>
+        <MobileMenu
+          links={linkElements}
+          menuOpen={menuOpen}
+          setMenuOpen={setMenuOpen}
+        />
+      </nav>
+    </>
   );
 };
 
