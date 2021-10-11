a2cc50996819eb51b5da130ca3ad4ce66234cd71
Sigurd <sigurbak@stud.ntnu.no>
2021-10-11 17:52:18 +0200

    Fixed static text and image

--- src/pages/public/Kontakt/components/Kontakt.tsx
+++ src/pages/public/Kontakt/components/Kontakt.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import KontaktCard from "components/Pages/KontaktCard";
+import getContent from "api/Kontakt";
 
 const Kontakt = (): JSX.Element => {
-  return <article className="container"><h1>Siden for kontakt</h1></article>;
+  const { title, card } = getContent();
+  return (
+    <div className="max-w-screen-lg mt-10 mb-20 mx-auto ml-2 flex flex-col items-center">
+      <KontaktCard
+        key={card.title}
+        title={card.title}
+        text={card.text}
+        alt={card.image.alt}
+        imgPath={card.image.url}
+      />
+      <h1 className="max-w-2xl text-gray-600 text-5xl text-center font-bold ml-40 mt-10 mb-10">
+        {title}
+      </h1>
+    </div>
+  );
 };
 
 export default Kontakt;
