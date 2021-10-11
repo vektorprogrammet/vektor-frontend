9e4738857b702246eb4c8d1a5a43cce9727cb99f
Sigurd <sigurbak@stud.ntnu.no>
2021-10-11 18:28:29 +0200

    Fixed bold text on active navlink

--- src/components/Header/AppHeader.tsx
+++ src/components/Header/AppHeader.tsx
@@ -1,21 +1,73 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const AppHeader = (): JSX.Element => {
   return (
     <nav className="AppHeader shadow-md">
       <div className="max-w-6xl mx-auto p-2 flex space-x-8 items-end">
-        <Link to="/">
-          <img src="images/vektor-logo.svg" alt="vektorprogrammet logo" className="h-20" />
-        </Link>
+        <NavLink to="/">
+          <img
+            src="images/vektor-logo.svg"
+            alt="vektorprogrammet logo"
+            className="h-20"
+          />
+        </NavLink>
         <div className="flex-grow" />
-        <Link className="header-link" to="/assistenter">Assistenter</Link>
-        <Link className="header-link" to="/om-vektor">Om oss</Link>
-        <Link className="header-link" to="/skoler">For skoler</Link>
-        <Link className="header-link" to="/foreldre">For foreldre</Link>
-        <Link className="header-link" to="/team">Team</Link>
-        <Link className="header-link" to="/kontakt">Kontakt</Link>
+        <NavLink
+          className="header-link"
+          to="/assistenter"
+          activeStyle={{
+            fontWeight: "bold",
+          }}
+        >
+          Assistenter
+        </NavLink>
+        <NavLink
+          className="header-link"
+          to="/om-vektor"
+          activeStyle={{
+            fontWeight: "bold",
+          }}
+        >
+          Om oss
+        </NavLink>
+        <NavLink
+          className="header-link"
+          to="/skoler"
+          activeStyle={{
+            fontWeight: "bold",
+          }}
+        >
+          For skoler
+        </NavLink>
+        <NavLink
+          className="header-link"
+          to="/foreldre"
+          activeStyle={{
+            fontWeight: "bold",
+          }}
+        >
+          For foreldre
+        </NavLink>
+        <NavLink
+          className="header-link"
+          to="/team"
+          activeStyle={{
+            fontWeight: "bold",
+          }}
+        >
+          Team
+        </NavLink>
+        <NavLink
+          className="header-link"
+          to="/kontakt"
+          activeStyle={{
+            fontWeight: "bold",
+          }}
+        >
+          Kontakt
+        </NavLink>
       </div>
     </nav>
   );
