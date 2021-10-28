f42deba844e0b12552ba7180f22d2aec4aca6940
Sigurd <sigurbak@stud.ntnu.no>
2021-10-28 17:36:23 +0200

    Fake api and teamcard

--- src/pages/public/Team/components/Team.tsx
+++ src/pages/public/Team/components/Team.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import getContent from "api/Team";
+import TeamCard from "./TeamCard";
 
 const Team = (): JSX.Element => {
-  return <article className="container"><h1>Siden for team</h1></article>;
+  const { title, card } = getContent();
+  return (
+    <div className="max-w-screen-lg mt-10 mb-20 mx-auto ml-2 flex flex-col items-center">
+      <TeamCard
+        key={card.title}
+        title={card.title}
+        text1={card.text1}
+        text2={card.text2}
+        text3={card.text3}
+        alt={card.image.alt}
+        imgPath={card.image.url}
+      />
+      <h1 className="max-w-2xl text-gray-600 text-5xl text-center font-bold ml-40 mt-10 mb-10">
+        {title}
+      </h1>
+    </div>
+  );
+  // return <article className="container"><h1>Siden for team</h1></article>;
 };
 
 export default Team;
---
+++ src/pages/public/Team/components/TeamCard.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+interface Props {
+  title: string;
+  text1: string;
+  text2: string;
+  text3: string;
+  imgPath: URL;
+  alt: string;
+}
+
+const TeamCard = ({
+  title,
+  text1,
+  text2,
+  text3,
+  alt,
+  imgPath,
+}: Props): JSX.Element => {
+  return (
+    <div className="float-container">
+      <div className="w-2/5 float-left max-w-screen-sm mt-10 mb-0 ml-10 flex flex-col">
+        <h1 className="max-w-2xl text-gray-600 text-4xl text-left font-bold mx-3">
+          {title}
+        </h1>
+        <p className="max-w-2xl text-left mt-4 mb-5 text-xl mx-3">{text1}</p>
+        <p className="max-w-2xl text-left text-xl mx-3">{text2}</p>
+        <p className="max-w-2xl text-left mb-20 text-xl mx-3 font-bold">
+          {text3}
+        </p>
+      </div>
+
+      <svg>
+        <polyline
+          fill="none"
+          strokeWidth="2"
+          stroke="#2b69b0"
+          //   class="city-polyline"
+          points="1500,1500 50,50 150,50"
+        />
+      </svg>
+      <img
+        src={imgPath.href}
+        className="float-right max-w-sm rounded-lg mt-10 mx-auto w-2/5"
+        alt={alt}
+      />
+    </div>
+  );
+};
+
+export default TeamCard;
