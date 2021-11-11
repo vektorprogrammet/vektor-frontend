a01541f4734595bbf3159798e994c888b023f906
HKrogstie <krogstie.havard@gmail.com>
2021-11-11 18:58:53 +0100

    [VF-18] feat: add footer content from figma and make dark mode toggle work (#123)
    * [VF-18] feat: add footer content from figma and make dark mode toggle work
    
    * [VF-18] fix: give new alt text to footer logo to fix test
    
    * fix(footer): use grid to align icons and text in footer
    
    * fix(darkMode): throw if setting dark mode when no DarkModeContext is provided
    
    * feat(footer+dark): indrease spacing like figma, and add smooth transitions for bg-color
    
    * Add hover-effect, add org. number
    
    * Branch up to speed with master
    
    * Fix comment in tailwind for darkMode options
    
    Co-authored-by: lundli <46197518+Lundli@users.noreply.github.com>
    Co-authored-by: hhtjo <hahotjo@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { DarkModeProvider } from "components/DarkModeProvider";
 import routes from "./pages/public/routes";
 import AppFooter from "./components/AppFooter/AppFooter";
 import AppHeader from "./components/Header/AppHeader";
@@ -8,29 +9,29 @@ import "./App.css";
 
 const App = (): JSX.Element => {
   return (
-    <BrowserRouter>
-      <div className="App flex flex-col items-stretch min-h-screen">
-        <AppHeader />
-        {/* Banner */}
-        <main className="flex-grow">
-          <Switch>
-            {/* Use component when the rendered component needs no props */}
-            {/* Getting the routes from the defined route file in pages */}
-            {routes.map((page_route) => (
-              <Route
-                key={page_route.route}
-                path={page_route.route}
-                exact
-                component={page_route.component}
-              />
-            ))}
-          </Switch>
-        </main>
-        <footer>
+    <DarkModeProvider>
+      <BrowserRouter>
+        <div className="App flex flex-col items-stretch min-h-screen">
+          <AppHeader />
+          {/* Banner */}
+          <main className="flex-grow">
+            <Switch>
+              {/* Use component when the rendered component needs no props */}
+              {/* Getting the routes from the defined route file in pages */}
+              {routes.map((page_route) => (
+                <Route
+                  key={page_route.route}
+                  path={page_route.route}
+                  exact
+                  component={page_route.component}
+                />
+              ))}
+            </Switch>
+          </main>
           <AppFooter />
-        </footer>
-      </div>
-    </BrowserRouter>
+        </div>
+      </BrowserRouter>
+    </DarkModeProvider>
   );
 };
 
