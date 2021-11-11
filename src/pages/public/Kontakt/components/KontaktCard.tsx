056835e9ba4910ff599c7be24cd16d2a21de7d18
Sigurd <sigurbak@stud.ntnu.no>
2021-11-11 19:59:25 +0100

    Fixed positioning on browser zoom

--- src/pages/public/Kontakt/components/KontaktCard.tsx
+++ src/pages/public/Kontakt/components/KontaktCard.tsx
@@ -8,20 +8,20 @@ interface Props {
 }
 
 const KontaktCard = ({
-  title, text, alt, imgPath,
+  title, text, imgPath, alt,
 }: Props): JSX.Element => {
   return (
-    <div className="float-container">
-      <div className="w-2/5 float-left max-w-screen-sm mt-5 mb-0 -ml-20 flex flex-col">
-        <h1 className="max-w-2xl text-gray-600 text-4xl text-left font-bold mx-3">
+    <div className="flex w-full mx-auto justify-around flex-wrap">
+      <div className="max-w-6xl mt-5 flex flex-col">
+        <h1 className=" text-gray-600 text-4xl font-bold mx-3">
           {title}
         </h1>
-        <p className="max-w-2xl text-left mt-4 mb-20 text-xl mx-3">{text}</p>
+        <p className="max-w-md mt-4 mb-20 text-xl mx-3">{text}</p>
       </div>
       <img
         src={imgPath.href}
-        className="float-right max-w-md rounded-lg mt-10 mr-45 w-2/5"
         alt={alt}
+        className="max-w-md rounded-lg mt-10 mx-auto max-h-44 mr-30"
       />
     </div>
   );
