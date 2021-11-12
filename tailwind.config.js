2c795bedc0d9b56642800bc040800491f34b7d2f
Erlend Marius Ommundsen <43030358+ErlendMariusOmmundsen@users.noreply.github.com>
2021-11-12 20:15:40 +0100

    [VF-41] mobile friendly header (#145)
    * [VF-41] Add mobile menu first draft
    
    * [VF-41] Add mobile menu styling
    
    * [VF-41] Modify Appheader for mobile menu
    
    * [VF-41] Refactor + remove unused css
    
    * [VF-41] Add custom tailwind media queries
    
    * [VF-41] Refactor hamburger animation
    
    * [VF-41] Fix responsive link appearance

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or false or 'media'
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '860px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {},
   },
   variants: {
