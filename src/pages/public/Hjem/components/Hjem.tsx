4f4b43500c53931e00adff774da3bcef3fae6e5a
thersi <therese.sigmundstad@gmail.com>
2022-02-02 16:30:11 +0100

    Add css

--- src/pages/public/Hjem/components/Hjem.tsx
+++ src/pages/public/Hjem/components/Hjem.tsx
@@ -1,10 +1,22 @@
 import "../hjem.css";
 const Hjem = (): JSX.Element => {
   return (
-    <div className="container">
-      <div className="imageContainer">
-        <img className="blackboardImg" src="images/blackboard.png" />;
+    <div className="outerContainer">
+      <div className="container">
+        <div className="imageContainer">
+          <img className="blackboardImg" src="images/blackboard.png" />;
+        </div>
+        <div className="infoContainer">
+          <h1>Vektorprogrammet</h1>
+          <p>
+            - sender studenter til ungdomsskoler for å hjelpe til som lærerens
+            assistent i matematikkundervisningen
+          </p>
+        </div>
       </div>
+      <p>
+        <h2>292</h2>
+      </p>
     </div>
   );
 };
