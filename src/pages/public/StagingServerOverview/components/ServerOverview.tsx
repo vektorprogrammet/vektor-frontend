fffa2d0e73b09449380fc3a208fdf458a8d3e620
Erlend Marius Ommundsen <emommundsen@gmail.com>
2022-02-23 18:59:27 +0100

    Add page preview + styling

--- src/pages/public/StagingServerOverview/components/ServerOverview.tsx
+++ src/pages/public/StagingServerOverview/components/ServerOverview.tsx
@@ -1,51 +1,87 @@
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+
 import { fetchServers, StagingServerAPIResponse } from "./FetchUtils";
+
+import "../../../../App.css";
 
 const ServerOverview = (): JSX.Element => {
   const [servers, setServers] = useState<[StagingServerAPIResponse]>();
+  const [rows, setRows] = useState<Array<Number>>([]);
 
   useEffect(() => {
     async function getServers() {
       const data = await fetchServers();
       setServers(data);
+      const l = new Array(data.length).fill(0);
+      setRows(l);
     }
     getServers();
   }, []);
 
-  console.log(servers);
+  const tableHeadClassName = "px-5 py-3 border-gray-200 bg-gray-300 text-left text-m font-bold text-gray-600 uppercase tracking-wider";
+
   return (
     <div>
-      <h1>Staging-servers</h1>
-      <table className="w-10/12 justify-center m-auto">
+      <table className="justify-center m-auto my-20">
         <thead>
           <tr>
             <th
-              className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+              className={tableHeadClassName}
+            >
               Repo
             </th>
             <th
-              className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+              className={tableHeadClassName}
+            >
               Branch
             </th>
             <th
-              className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+              className={tableHeadClassName}
+            >
               URL
             </th>
             <th
-              className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-              Visit
+              className={tableHeadClassName}
+            >
+              Link
             </th>
           </tr>
         </thead>
         <tbody>
-          {servers?.map((server) => {
+          {servers?.map((server, index) => {
             return (
-              <tr>
-                <td>{server.branch}</td>
-                <td>{server.repo.substring(("https://github.com/vektorprogrammet/").length)}</td>
-                <td>{server.url}</td>
-                <td><a href={server.url}>Visit</a></td>
-              </tr>
+              <>
+                <tr
+                  key={server.branch}
+                  onClick={() => {
+                    // This deep-copies the array which makes the component rerender
+                    const rowsCopy = JSON.parse(JSON.stringify(rows));
+                    rowsCopy[index] = rowsCopy[index] === 0 ? 1 : 0;
+                    setRows(rowsCopy);
+                  }}
+                  className={`${index % 2 === 1 ? "bg-gray-900 bg-opacity-10" : ""}`}
+                >
+                  <td className="p-5">{server.branch}</td>
+                  <td className="p-5">{server.repo.substring(("https://github.com/vektorprogrammet/").length)}</td>
+                  <td className="p-5">{server.url}</td>
+                  <td className="p-5">
+                    <a href={server.url} target="_blank" rel="noreferrer">
+                      <button onClick={(e) => e.stopPropagation()} type="button" className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
+                        <FontAwesomeIcon icon={faExternalLinkAlt} />
+                      </button>
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan={4}>
+                    {rows[index] === 1
+                      ? <iframe className="w-full h-96 border-b-2 border-r-2 border-l-2 border-blue-400" title="preview" src={server.url} frameBorder="1" />
+                      : null}
+                  </td>
+                </tr>
+              </>
             );
           })}
         </tbody>
