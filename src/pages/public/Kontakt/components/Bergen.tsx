eb6c4c583bd9cb5b20515ff3b53c8d847e85c315
Heidi Wiest <heiwie@hotmail.com>
2022-08-31 19:00:44 +0200

    [VF-55] added google recaptcha component

--- src/pages/public/Kontakt/components/Bergen.tsx
+++ src/pages/public/Kontakt/components/Bergen.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const Bergen = (): JSX.Element => {
   return (
@@ -62,7 +63,10 @@ const Bergen = (): JSX.Element => {
               <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </label>
           </div>
-          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send melding</button>
+          <ReCAPTCHA
+            sitekey="Your client site key"
+          />
+          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-3 left-0">Send melding</button>
         </form>
       </div>
     </>
--- src/pages/public/Kontakt/components/Trondheim.tsx
+++ src/pages/public/Kontakt/components/Trondheim.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const Trondheim = (): JSX.Element => {
   return (
@@ -89,7 +90,10 @@ const Trondheim = (): JSX.Element => {
               <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300" />
             </label>
           </div>
-          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send melding</button>
+          <ReCAPTCHA
+            sitekey="Your client site key"
+          />
+          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-3">Send melding</button>
         </form>
       </div>
 
