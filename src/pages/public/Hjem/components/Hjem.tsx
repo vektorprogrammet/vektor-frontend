59526027816a0bfc99ad5aeeb0fdbb078aabc455
thersi <therese.sigmundstad@gmail.com>
2022-09-28 18:57:15 +0200

    Add new format to home page

--- src/pages/public/Hjem/components/Hjem.tsx
+++ src/pages/public/Hjem/components/Hjem.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "../hjem.css";
 
 const Hjem = (): JSX.Element => {
@@ -5,32 +6,26 @@ const Hjem = (): JSX.Element => {
     <div className="w-full">
       <img className="pt-600 w-full" src="images/Rectangle.png" />
       <div className="absolute w-full">
-        <div className="absolute flex-column w-full pb-180 pl-5 py-20 bottom-0 inset-x-0 text-black text-xs text-left leading-4 align-text-top ">
-          <h1 className="text-dark-blue-vektor text-4xl ">Vektorprogrammet</h1>
-          <p className="text-2xl tracking-wide pb-10">
+        <div className="absolute flex-column w-full pl-20 py-80 bottom-0 inset-x-0 text-vektor-darblue text-xs text-left leading-4 align-text-top ">
+          <h1 className="vektor-darblue text-4xl ">Vektorprogrammet</h1>
+          <p className="text-xl max-w-sm leading-6 tracking-wide pb-10">
             - sender studenter til ungdomsskoler for å hjelpe til som lærerens
             assistent i matematikkundervisningen
           </p>
-          <div className="flex flex-row space-x-2">
-            <div className="flex">
-              <a
-                ///href="#"
-                className="px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-dark-blue-vektor hover:bg-medium-blue-vektor md:py-4 md:text-lg md:px-10"
+          <div className="flex flex-row space-x-2 ">
+            <div className="flex space-x-4 w-1/5 justify-start">
+              <button
+                type="button"
+                className="px-8 py-3 bg-vektor-darblue text-base font-medium hover:bg-blue-400 text-white px-6 rounded-full"
               >
-                Get started
-              </a>
+                Les mer og bli assistent
+              </button>
             </div>
-            <a
-              ///href="#"
-              className="px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-dark-blue-vektor hover:bg-medium-blue-vektor md:py-4 md:text-lg md:px-10"
-            >
-              Get started
-            </a>
           </div>
         </div>
       </div>
-      <div className="flex flex-ro justify-center">
-        <div className="">
+      <div className="pl-20 pt-10 grid grid-cols-2 gap-2">
+        <div>
           <h1>1755</h1>
           <h2>Assistenter</h2>
           <h3>
@@ -38,16 +33,24 @@ const Hjem = (): JSX.Element => {
             Vektorprogrammet.
           </h3>
         </div>
-        <div className="">
+        <div>
           <h1>292</h1>
           <h2>Assistenter</h2>
           <h3>
-            Over 1755 studenter har hatt et verv som vektorassistent i
-            Vektorprogrammet.
+            sender studenter til ungdomsskoler for å hjelpe til som lærerens
+            assistent i matematikkundervisningen
           </h3>
         </div>
+        <h2>test</h2>
+        <div className="flex flex-row pt-20 place-content-left">
+          <img className="blackboardImg" src="images/blackboard.png" />
+        </div>
       </div>
-      <div className="sponsors">
+      <h1 className="flex justify-center pt-20">Sponsorer</h1>
+      <div className="pt-10 grid grid-cols-3 gap-4">
+        <img className="teknaImg" src="images/tekna.jpg" />
+        <img className="sponsorImg" src="images/ntnu1.png" />
+        <img className="sponsorImg" src="images/ntnu2.png" />
         <img className="teknaImg" src="images/tekna.jpg" />
         <img className="sponsorImg" src="images/ntnu1.png" />
         <img className="sponsorImg" src="images/ntnu2.png" />
