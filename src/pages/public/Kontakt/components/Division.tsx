850a25a8771ee374f0f81fb4b62cfe13e6fc6a5b
Heidi Wiest <heiwie@hotmail.com>
2022-10-25 17:12:25 +0200

    [VF-55] removed google recaptcha

--- src/pages/public/Kontakt/components/Division.tsx
+++ src/pages/public/Kontakt/components/Division.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReCAPTCHA from "react-google-recaptcha";
 
 interface DivisionProps {
   name: string;
@@ -107,9 +106,6 @@ const Division = ({
               <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300" />
             </label>
           </div>
-          <ReCAPTCHA
-            sitekey="Your client site key"
-          />
           <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-3">Send melding</button>
         </form>
       </div>
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,4 +1,1 @@
 /// <reference types="react-scripts" />
-declare module "react/jsx-runtime" {
-  export default any;
-}
