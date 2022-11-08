0f66043929366313f3ccfc563148f8cdf08446dd
Heidi Wiest <heiwie@hotmail.com>
2022-11-08 18:28:05 +0100

    [VF-72] fixed design tab

--- src/pages/public/Assistenter/components/CityCard/Citycard.tsx
+++ src/pages/public/Assistenter/components/CityCard/Citycard.tsx
@@ -11,15 +11,14 @@ const Tab = (
     city: City,
     open: boolean },
 ): JSX.Element => {
-  const chosenStyle = open ? `border-t-gray-200 border-l-gray-200 border-r-gray-200 border-b-0 text-vektor-darblue hover:border-b-0` : `text-vektor-darblue`;
+  const chosenStyle = open ? `border-t-gray-200 border-l-gray-200 border-r-gray-200 border-b-white text-vektor-darblue z-50` : `text-vektor-darblue z-50`;
   return (
-    <div className="flex-auto text-center border-gray-200 border-b-2 -mb-[2px] ">
+    <div className="flex-auto text-center border-gray-200 border-b-2 -mb-[2px] z-50">
       <button
         type="button"
-        className={`text-base w-full font-bold py-3 border-b-gray-200 hover:border-2 hover:border-gray-200 hover:text-vektor-blue border-2 border-b-2 border-white ${chosenStyle}`}
+        className={`rounded-t-lg -mb-[2px] z-50 text-base w-full font-bold py-3 border-b-gray-200 hover:border-2 hover:border-gray-200 hover:text-vektor-blue border-2 border-b-2 border-white ${chosenStyle}`}
         onClick={onTabClick}
         data-toggle="tab"
-                  //  href={`#link${data.number}`}
         role="tablist"
       >
         {city}
@@ -42,7 +41,7 @@ const Tabs = (): JSX.Element => {
             />
           ))}
         </div>
-        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 border-b-2 border-r-2 border-l-2 px-4 py-5 flex-auto tab-content tab-space">
+        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 border-b-2 border-r-2 border-l-2 px-4 py-5 flex-auto tab-content tab-space z-40">
           <ApplyReg cities={openTab} />
         </div>
       </div>
