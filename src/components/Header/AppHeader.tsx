ff0192627a6b536f298da77e57bc05f310109373
supergelf <54675048+supergelf@users.noreply.github.com>
2022-11-15 18:40:29 +0100

    [VF-63] add user avatar (#184)
    * [VF-63] add user avatar
    
    * Check if user is logged in
    
    * [VF-63] add user avatar fixed
    
    Co-authored-by: hhtjo <hahotjo@gmail.com>

--- src/components/Header/AppHeader.tsx
+++ src/components/Header/AppHeader.tsx
@@ -7,38 +7,87 @@ const activeStyle: React.CSSProperties = {
   fontWeight: "bold",
 };
 
+const UserAvatar = () => {
+  return (
+    <button tabIndex={0} className="dropdown dropdown-end" type="button">
+      <div className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            src="https://placeimg.com/80/80/people"
+            alt="profile avatar"
+          />
+        </div>
+      </div>
+      <ul className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a href="#0">Kontrollpanel</a>
+        </li>
+        <li>
+          <a href="#0">Min side</a>
+        </li>
+        <li>
+          <a href="#0">Profil</a>
+        </li>
+        <li>
+          <a href="#0">Mine utlegg</a>
+        </li>
+        <li className="text-red-500">
+          <a href="#0">Logg ut</a>
+        </li>
+      </ul>
+    </button>
+  );
+};
+
+const LoginButtons = () => {
+  return (
+    <div className="flex space-x-4">
+      <button
+        type="button"
+        className="bg-blue-900 hover:bg-gray-800 text-white px-4 py-2 rounded-full duration-300"
+      >
+        Jeg er ny
+      </button>
+      <button
+        type="button"
+        className="bg-blue-400 hover:bg-blue-900 text-white px-4 py-2 rounded-full duration-300"
+      >
+        Logg inn
+      </button>
+    </div>
+  );
+};
+
 const AppHeader = (): JSX.Element => {
   const [menuOpen, setMenuOpen] = useState(false);
-
-  const linkElements = routes.map(
-    (route) => (
-      <NavLink
-        key={route.route}
-        // className="mt-4 vektor-font font-semibold p-1"
-        className="header-link mobile-link"
-        to={route.route}
-        style={({ isActive }) => (isActive ? activeStyle : {})}
-      >
-        {route.route[1].toUpperCase() + route.route.substring(2).replace("-", " ")}
-      </NavLink>
-    ),
-  );
+  const isLoggedIn = false;
+  const linkElements = routes.map((route) => (
+    <NavLink
+      key={route.route}
+      // className="mt-4 vektor-font font-semibold p-1"
+      className="header-link mobile-link"
+      to={route.route}
+      style={({ isActive }) => (isActive ? activeStyle : {})}
+    >
+      {route.route[1].toUpperCase()
+        + route.route.substring(2).replace("-", " ")}
+    </NavLink>
+  ));
 
   return (
     <nav className="AppHeader md:sticky top-0 shadow-sm z-50">
-      <div className="hidden md:flex mx-auto p-3 space-x-8 items-center justify-center lg:px-8 w-full max-w-7xl">
+      <div className="hidden md:flex mx-auto p-3 space-x-6 items-center justify-center lg:px-8 w-full max-w-7xl">
         <div className="w-1/5 justify-center flex">
           <Link to="/">
             <img src="images/vektor-logo.svg" alt="vektorprogrammet logo" className="h-16 lg:h-20 dark:hidden" />
             <img src="images/vektor-logo-darkmode.png" alt="vektorprogrammet logo" className="h-16 lg:h-20 hidden dark:block" />
           </Link>
         </div>
-        <div className="flex flex-grow justify-between justify-self-center max-w-lg m-auto items-center w-2/5">
+        <div className="flex flex-grow justify-evenly justify-self-center max-w-lg m-auto items-center w-2/5">
           {linkElements}
         </div>
-        <div className="flex space-x-4 w-1/5 justify-start">
-          <button type="button" className="bg-vektor-darblue hover:bg-vektor-blue-hover text-white px-6 py-2 rounded-full">Jeg er ny</button>
-          <button type="button" className="bg-vektor-darblue hover:bg-vektor-blue-hover text-white px-6 py-2 rounded-full">Logg inn</button>
+        <div className="flex w-1/5 justify-center">
+          {isLoggedIn ? <UserAvatar /> : <LoginButtons />}
         </div>
       </div>
       <MobileMenu
