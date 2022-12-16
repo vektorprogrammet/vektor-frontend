9baa75f5616045cd3e0cc6a51aae7e62da075562
Trym Tveito <56224255+trymtv@users.noreply.github.com>
2022-12-16 22:13:41 +0100

    VF-74 Vitest (#191)
    * feat: add vitest
    
    * feat: add vitest to pipeline
    
    * fix: add correct scripts and disable cypress videos

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders logo img", async () => {
-  render(<App />);
-  const linkElement = await screen.findByAltText("vektorprogrammet logo");
-  expect(linkElement.tagName).toBe("IMG");
-});
