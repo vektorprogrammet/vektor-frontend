eb6fbf4a3ef198957aad9260ddddf670ba5bc41b
Heidi <101273925+heidiwiest@users.noreply.github.com>
2023-09-13 19:08:44 +0200

    added profile picture

--- src/pages/public/Kontrollpanel/components/SideBar.tsx
+++ src/pages/public/Kontrollpanel/components/SideBar.tsx
@@ -2,9 +2,21 @@ import React from "react";
 
 const SideBar = (): JSX.Element => {
   return (
-    <div className="border-2 bg-red-500 basis-48 h-full flex flex-col items-center">
-      <div className="h-20 w-20 bg-red-100 m-8">
-        logo
+    <div className="border-2 bg-gray-700 w-48 h-full flex flex-col items-center">
+      <div className="bg-gray-700 m-8" id="dropdownContainer">
+        <div className="dropdown-trigger">
+          <img
+            src="https://vektorprogrammet.no/media/cache/profile_img/images/Profile%20photos/644805d1e8ef2.jpeg"
+            className="rounded-full"
+            alt="vektorbilde"
+          />
+          <div className="text-white">Aaryan Neupane</div>
+        </div>
+        <div className="dropdown-content hidden" id="dropdownMenu">
+          <a href="#">Item 1</a>
+          <a href="#">Item 2</a>
+          <a href="#">Item 3</a>
+        </div>
       </div>
       <div className="flex flex-col items-center gap-8">
         <div className="h-14 w-14 bg-gray-200 border-0">
