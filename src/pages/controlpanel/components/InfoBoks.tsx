e01c92eeafa16ed598e5680a38687cdc0a8d4586
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-10-05 18:46:33 +0200

    #120 infoboks finished

--- src/pages/controlpanel/components/InfoBoks.tsx
+++ src/pages/controlpanel/components/InfoBoks.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
 
-interface InfoBoksProps {
-  column1: string | undefined;
-  column2: string | undefined;
-  column3: string | undefined;
-  column4: string | undefined;
-  column5: string | undefined;
-  column6: string | undefined;
-  column7: string | undefined;
-  column8: string | undefined;
-}
-
-const InfoBoks = ({ column1, column2, ...rest }: InfoBoksProps) => {
-
+const InfoBoks = ({ info }: { info: string[] }) => {
   return (
-    <div>
-      <p> Heisann</p>
+    <div className="flex gap-14 bg-vektor-blue rounded-3xl justify-around h-20 shadow-md shadow-gray-500">
+      {info.map((infoCell, index) => (
+        <p key={index} className="flex items-center justify-center">
+          {infoCell}
+        </p>
+      ))}
     </div>
-
-  )
-
-}
+  );
+};
 
 export default InfoBoks;
--- src/pages/controlpanel/index.tsx
+++ src/pages/controlpanel/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import SideBar from "./components/SideBar";
-import InfoBoks from "./components/InfoBoks";
 
 const ControlPanel = (): JSX.Element => {
   return (
     <div className="bg-gray-100 flex flex-row h-screen">
-      <InfoBoks column1="Donald Duck" column2="donald.duck@disney.na"  column3="90150056" column4="MTDT" column5="Trondheim" column6="" column7="" column8=""/>
       <SideBar />
       <Outlet />
     </div>
