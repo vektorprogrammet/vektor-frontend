f9c4d4852dc2dc0f2f5a5435dd9a11fa5ff32387
Heidi <101273925+heidiwiest@users.noreply.github.com>
2023-10-05 16:40:48 +0200

    Fixed merging issues

--- src/pages/public/Kontrollpanel/components/Kontrollpanel.tsx
+++ src/pages/public/Kontrollpanel/components/Kontrollpanel.tsx
@@ -1,15 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import SideBar from "./SideBar";
 
 const Kontrollpanel = (): JSX.Element => {
+  // dummy data for buttons on control panel home page
+  const mainButtons: Array<string> = [
+    "Opptak",
+    "Stillinger",
+    "Team",
+    "Avdelinger",
+    "Økonomi",
+  ];
+
+  const infoCards: Array<string> = [
+    "Søkere",
+    "Nyheter",
+  ];
+
+  // Map control page pages to main buttons
+  const buttonElements = mainButtons.map((mainButton) => (
+    <Link
+      reloadDocument
+      key={mainButton.toLowerCase()}
+      to={mainButton.toLowerCase()}
+      className="w-40 h-40 bg-vektor-blue flex items-center justify-center rounded-lg font-bold hover:bg-vektor-darblue hover:text-white duration-200 shadow-lg hover:mb-2"
+    >
+      {mainButton}
+    </Link>
+  ));
+
+  const infoElements = infoCards.map((infoCard) => (
+    <div key={infoCard} className="w-2/5 h-5/6 bg-gray-400 flex items-center justify-center rounded-lg font-bold">
+      {infoCard}
+    </div>
+  ));
+
   return (
-    <div className="border-2 border-black flex flex-row">
-      <SideBar />
+    <div className="flex flex-row items-end flex-grow">
+      <SideBar/>
+      <div className="basis-full h-full flex flex-col text-center">
+        <div className="h-1/2 flex flex-row items-center justify-center px-12 gap-16">
+          {buttonElements}
+        </div>
 
-      <div className="border-2 bg-red-200 basis-full h-full flex flex-col text-center">
-        <div className="h-2/5 bg-gray-200" />
-
-        <div className="h-3/5" />
+        <div className="h-1/2 flex flex-row items-top justify-center px-12 gap-12">
+          {infoElements}
+        </div>
       </div>
     </div>
 
