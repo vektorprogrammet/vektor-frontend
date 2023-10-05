591f832965250431bab4807d92f77f00560a0174
Heidi <101273925+heidiwiest@users.noreply.github.com>
2023-10-05 18:51:34 +0200

    Gjorde komponenten mer responsiv

--- src/pages/controlpanel/components/SideBar.tsx
+++ src/pages/controlpanel/components/SideBar.tsx
@@ -1,11 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   BrukereIcon, AssistentIcon, OpptakIcon, OpptaksstatistikkIcon, SkoleIcon, TeamIcon, TeamInteresseIcon,
 } from "./Icons";
-import { Link } from "react-router-dom";
 
 const HorizontalLine = (): JSX.Element => {
-  return <hr className="border-t-2 border-white w-4/5 mt-4" />;
+  return <hr className="border-t-2 mb-2 border-white w-4/5 mt-4" />;
 };
 
 interface IconProps {
@@ -13,13 +13,23 @@ interface IconProps {
   name: string;
 }
 
+const linkData = [
+  { to: "brukere", icon: <BrukereIcon />, name: "Brukere" },
+  { to: "assistenter", icon: <AssistentIcon />, name: "Assistenter" },
+  { to: "opptak", icon: <OpptakIcon />, name: "Opptak" },
+  { to: "opptaksstatistikk", icon: <OpptaksstatistikkIcon />, name: "Opptaksstatistikk" },
+  { to: "skoler", icon: <SkoleIcon />, name: "Skoler" },
+  { to: "team", icon: <TeamIcon />, name: "Team" },
+  { to: "teaminteresse", icon: <TeamInteresseIcon />, name: "Teaminteresse" },
+];
+
 const Icon: React.FC<IconProps> = ({ svg, name }) => {
   return (
-    <div className="flex flex-col items-center justify-center my-2 sm:my-3 md:my-4">
-      <button type="button" className="bg-vektor-blue hover:bg-white rounded-full flex items-center justify-center w-8 h-8 sm:w-11 sm:h-11 md:w-14 md:h-14">
+    <div className="flex flex-col items-center justify-center my-2">
+      <button type="button" className="bg-vektor-blue hover:bg-white rounded-full flex items-center justify-center w-8 h-8 sm:w-11 sm:h-11">
         {svg}
       </button>
-      <div className="text-center text-white mt-1 text-xs md:text-sm mx-2 truncate w-20 sm:w-28 md:w-36">
+      <div className="text-center text-white text-xs md:text-sm mx-2 truncate w-20 sm:w-28 md:w-36">
         {name}
       </div>
     </div>
@@ -27,11 +37,10 @@ const Icon: React.FC<IconProps> = ({ svg, name }) => {
 };
 
 const SideBar = (): JSX.Element => {
-
   return (
-    <div className="bg-vektor-kontrollblue border-8 rounded-3xl w-28 sm:w-40 md:w-48 h-full flex flex-col items-center justify-start fixed">
+    <div className="bg-vektor-kontrollblue border-8 rounded-3xl w-24 sm:w-32 md:w-40 h-full flex flex-col items-center justify-start fixed overflow-y-auto overflow-x-hidden">
       <div
-        className="bg-white rounded-full flex items-center justify-center mt-4 w-16 h-16 sm:w-24 sm:h-24 md:w-32 md:h-32"
+        className="bg-white rounded-full flex items-center justify-center mt-2 w-16 h-16 sm:w-24 sm:h-24"
       >
         <img
           src="/images/vektor-logo-circle.svg"
@@ -39,13 +48,11 @@ const SideBar = (): JSX.Element => {
         />
       </div>
       <HorizontalLine />
-      <Link to="brukere"> <Icon svg={<BrukereIcon />} name="Brukere" /> </Link>
-      <Link to="assistenter"> <Icon svg={<AssistentIcon />} name="Assistenter" /> </Link>
-      <Link to="opptak"> <Icon svg={<OpptakIcon />} name="Opptak" /> </Link>
-      <Link to="opptaksstatistikk"> <Icon svg={<OpptaksstatistikkIcon />} name="Opptaksstatistikk" /> </Link>
-      <Link to="skoler"> <Icon svg={<SkoleIcon />} name="Skoler" /> </Link>
-      <Link to="team"> <Icon svg={<TeamIcon />} name="Team" /> </Link>
-      <Link to="teaminteresse"> <Icon svg={<TeamInteresseIcon />} name="Teaminteresse" /> </Link>
+      {linkData.map((item) => (
+        <Link to={item.to}>
+          <Icon svg={item.icon} name={item.name} />
+        </Link>
+      ))}
     </div>
   );
 };
