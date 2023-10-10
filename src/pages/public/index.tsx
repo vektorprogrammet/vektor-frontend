8b82170d7ff4ef5805b39492838440f3c5b52ccd
heidiwiest <101273925+heidiwiest@users.noreply.github.com>
2023-10-10 18:51:48 +0200

    La til ScrollRestoration komponent (#246)
    * La til ScrollRestoration komponent
    
    * Fixed lint issues

--- src/pages/public/index.tsx
+++ src/pages/public/index.tsx
@@ -1,24 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import AppFooter from "components/AppFooter/AppFooter";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import AppHeader from "components/Header/AppHeader";
-
-// ScrollToTop Component
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-};
 
 const MainPage = (): JSX.Element => {
   return (
     <div className="App flex flex-col items-stretch min-h-screen">
-      <ScrollToTop />
       <AppHeader />
+      <ScrollRestoration />
       {/* Banner */}
       <main className="flex-grow">
         {/* Use component when the rendered component needs no props */}
