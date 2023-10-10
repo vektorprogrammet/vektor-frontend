b0a02f07da4e645ccd175282348802e6a8c2fdd8
Heidi <101273925+heidiwiest@users.noreply.github.com>
2023-10-10 18:43:29 +0200

    Fixed lint issues

--- src/pages/public/index.tsx
+++ src/pages/public/index.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import AppFooter from "components/AppFooter/AppFooter";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import AppHeader from "components/Header/AppHeader";
-import { ScrollRestoration } from "react-router-dom";
 
 const MainPage = (): JSX.Element => {
   return (
     <div className="App flex flex-col items-stretch min-h-screen">
       <AppHeader />
-      <ScrollRestoration/>
+      <ScrollRestoration />
       {/* Banner */}
       <main className="flex-grow">
         {/* Use component when the rendered component needs no props */}
