7118b4894508e0c5dc0c4118eb69c3c60b4c4a00
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-10-29 14:54:30 +0100

    vf-111 opprett team side

--- src/pages/controlpanel/routes.tsx
+++ src/pages/controlpanel/routes.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Kontrollpanel from "pages/controlpanel/components/Kontrollpanel";
 import { AppRoute } from "App";
 import Skoler from "pages/controlpanel/components/Skoler";
+import OpprettTeam from "./components/OpprettTeam";
 
 // The route with the corresponding component to render in the route
 const routes: AppRoute[] = [
   { path: "", element: <Kontrollpanel />, name: "Kontrollpanel" },
   { path: "/kontrollpanel/skoler", element: <Skoler />, name: "Skoler" },
+  { path: "/kontrollpanel/teams/oppretteam", element: <OpprettTeam/>, name: "Opprett nytt team" },
 ];
 
 export default routes;
