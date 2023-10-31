46d6f79cb132ba1969e45896d7e693aec5cae72d
Filip Skaug <filis@stud.ntnu.no>
2023-10-31 18:26:24 +0100

    vf-126 fix map key

--- src/pages/controlpanel/components/Navbar.tsx
+++ src/pages/controlpanel/components/Navbar.tsx
@@ -11,6 +11,7 @@ const Navbar:React.FC<Props> = ({ select, selected, setSelected }) => {
     <>
       {select.map((item: string) => (
         <button
+          key={item}
           className={`row-start-2 col-start-1 pr-2 pl-2 pt-8 pb-2 ${selected === item ? "border-vektor-blue-hover border-b-4 pb-1" : ""}`}
           onClick={() => setSelected(item)}
           type="button"
