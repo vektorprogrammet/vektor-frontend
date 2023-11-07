e254f3a14753ac84fd2dc26f8b98363f7855c979
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-11-07 17:04:49 +0100

    VF-111 fix style css
    Fix styling css

--- src/pages/controlpanel/components/OpprettTeam.tsx
+++ src/pages/controlpanel/components/OpprettTeam.tsx
@@ -1,12 +1,13 @@
-import react from "react";
+import { useState } from "react";
 
 const OpprettTeam = (): JSX.Element => {
+
+  const [distrikt, setDistrikt] = useState<string>("Velg distrikt");
+
   return (
-    <div className="flex flex-col space-between w-full">
-      <div className="flex justify-center font-bold mt-10 text-4xl">
-        <h1>Opprett nytt team</h1>
-      </div>
-      <div className="ml-44 mt-10 flex text-2xl">
+    <div className="flex flex-col items-center space-between w-full">
+      <h1 className="text-2xl row-start-1 row-end-1 pt-10">Opprett team</h1>
+      <div className="mt-10 flex text-2xl">
         <div className="flex flex-col gap-10">
           <label>Navn:</label>
           <label>Distrikt:</label>
@@ -16,18 +17,24 @@ const OpprettTeam = (): JSX.Element => {
         </div>
         <div className="ml-10 flex flex-col gap-6">
           <input type="text" placeholder="Teamnavn" className="input input-bordered input-info w-full max-w-xs" />
-          <div className="dropdown dropdown-hover">
-            <label tabIndex={0} className="btn bg-vektor-blue m-1 ">Distrikt v</label>
-            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow rounded-box w-52">
-              <li><a>Trondheim</a></li>
-              <li><a>Bergen</a></li>
-              <li><a>Aas</a></li>
+          <button tabIndex={0} className="dropdown dropdown-end" type="button">
+              <p className="bg-white rounded-full text-m shadow">{distrikt}</p>
+            <ul className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+              <li>
+                <a onClick={() => setDistrikt("Trondheim")}>Trondheim</a>
+              </li>
+              <li>
+                <a onClick={() => setDistrikt("Ås")}>Ås</a>
+              </li>
+              <li>
+                <a onClick={() => setDistrikt("Bergen")}>Bergen</a>
+              </li>
             </ul>
-          </div>
+          </button>
           <input type="text" name="epost" placeholder="E-post" className="input input-bordered input-info w-full max-w-xs" />
           <input placeholder="Beskrivelse" className="input input-bordered input-info w-full max-w-xs" />
           <textarea placeholder="Beskrivelse" className="input input-bordered input-info w-full max-w-xs" />
-          
+
           <button type="button" className="login-buttons bg-blue-400 hover:bg-blue-900 text-white px-4 py-2 rounded-full duration-300">
             Opprett team
           </button>
