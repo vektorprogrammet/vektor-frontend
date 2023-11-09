33fd0e2a9c5821cd21b9cd92b3ff040de35435a4
Filip Skaug <filis@stud.ntnu.no>
2023-11-09 18:15:15 +0100

    vf-101 created skeleton

--- src/pages/controlpanel/components/Assistent.tsx
+++ src/pages/controlpanel/components/Assistent.tsx
@@ -1,23 +1,20 @@
 import React, { useState } from "react";
-
-  // interface Selected {
-  //   aktiveSkoler: boolean;
-  //   inaktiveSkoler: boolean;
-  // }
+import Navbar from "./Navbar";
 
 const Assistent = (): JSX.Element => {
-  const [active, setActive] = useState<boolean>(true);
-  const mapToTable = (skoler: School[]) => {
-    return skoler.map((school, index) => (
-      <tr key={index.valueOf()} className="even:bg-white odd:bg-table-grey">
-        <td className="text-center py-3 px-6">{school.name}</td>
-        <td className="text-center py-3 px-6">{school.contactPerson}</td>
-        <td className="text-center py-3 px-6">{school.email}</td>
-        <td className="text-center py-3 px-6">{school.phone}</td>
-        <td className="text-center py-3 px-6">{school.language}</td>
-      </tr>
-    ));
-  };
+  const select = ["Assistenter", "Vikarer"]
+  const [selected, setSelected] = useState<string>(select[0]);
+  // const mapToTable = (skoler: School[]) => {
+  //   return skoler.map((school, index) => (
+  //     <tr key={index.valueOf()} className="even:bg-white odd:bg-table-grey">
+  //       <td className="text-center py-3 px-6">{school.name}</td>
+  //       <td className="text-center py-3 px-6">{school.contactPerson}</td>
+  //       <td className="text-center py-3 px-6">{school.email}</td>
+  //       <td className="text-center py-3 px-6">{school.phone}</td>
+  //       <td className="text-center py-3 px-6">{school.language}</td>
+  //     </tr>
+  //   ));
+  // };
 
   interface School {
     name: string;
@@ -62,31 +59,14 @@ const Assistent = (): JSX.Element => {
   ];
 
   return (
-  <div className="w-screen">
-    <div className="pt-10 pr-10 pl-10 mt-50 shadow grid-rows-2 grid-cols-2 flex flex-col items-center">
-      <h1 className="text-2xl row-start-1 row-end-1">Assistenter</h1>
-      <div className="flex">
-        <button
-          className={`row-start-2 col-start-1 pr-2 pl-2 pt-8 pb-2 ${
-            active ? "border-vektor-blue-hover border-b-4 pb-1" : ""
-          }`}
-          onClick={() => setActive(true)}
-          type="button"
-        >
-          Assistenter
-        </button>
-        <button
-          className={`row-start-2 col-start-1 pr-2 pl-2 pt-8 pb-2 ${
-            !active ? "border-vektor-blue-hover border-b-4 pb-1" : ""
-          }`}
-          onClick={() => setActive(false)}
-          type="button"
-        >
-          Vikarer
-        </button>
+    <div className="w-full">
+      <div className="pt-10 pr-10 pl-10 mt-50 shadow grid-rows-2 grid-cols-2 flex flex-col items-center">
+        <h1 className="text-2xl row-start-1 row-end-1">Assistenter</h1>
+        <div className="flex flex-wrap ml-16">
+          <Navbar select={select} selected={selected} setSelected={setSelected} />
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
