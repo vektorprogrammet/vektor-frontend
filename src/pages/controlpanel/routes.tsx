382ca618f240014a14c01c16bb6628c68c6d44a0
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2023-11-09 17:24:29 +0100

    Added route to register.

--- src/pages/controlpanel/routes.tsx
+++ src/pages/controlpanel/routes.tsx
@@ -3,12 +3,14 @@ import Kontrollpanel from "pages/controlpanel/components/Kontrollpanel";
 import { AppRoute } from "App";
 import Skoler from "pages/controlpanel/components/Skoler";
 import Teaminteresse from "pages/controlpanel/components/Teaminteresse";
+import RegistrerSoker from "./components/RegistrerSoker";
 
 // The route with the corresponding component to render in the route
 const routes: AppRoute[] = [
   { path: "", element: <Kontrollpanel />, name: "Kontrollpanel" },
   { path: "/kontrollpanel/skoler", element: <Skoler />, name: "Skoler" },
   { path: "/kontrollpanel/teaminteresse", element: <Teaminteresse />, name: "Teaminteresse" },
+  { path: "/kontrollpanel/registrer-soker", element: <RegistrerSoker />, name: "Registrer søker"}
 ];
 
 export default routes;
