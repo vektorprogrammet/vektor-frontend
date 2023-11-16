1e3e9dba8c8129c23c8b5c0de2c39c791b17607d
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-11-16 18:31:30 +0100

    VF-111 Minor fixes opprettTeam side

--- src/pages/controlpanel/components/OpprettTeam.tsx
+++ src/pages/controlpanel/components/OpprettTeam.tsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 const OpprettTeam = (): JSX.Element => {
 
-  const [distrikt, setDistrikt] = useState<string>("Velg distrikt");
+  const [bold, setBold] = useState(false);
+
+  const handleBoldChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setBold(event.target.checked);
+  };
 
   return (
     <div className="flex flex-col items-center space-between w-full">
       <h1 className="text-2xl row-start-1 row-end-1 pt-10">Opprett team</h1>
-      <div className="mt-10 flex text-2xl">
+      <div className="mt-32 flex text-2xl">
         <div className="flex flex-col gap-12 text-xl">
           <label>Navn:</label>
           <label>Distrikt:</label>
@@ -16,7 +20,7 @@ const OpprettTeam = (): JSX.Element => {
           <label>Lang beskrivelse:</label>
         </div>
         <div className="ml-10 flex flex-col gap-6">
-          <input type="text" placeholder="Teamnavn" className="input input-bordered input-info w-full max-w-xs" />
+          <input type="text" placeholder="Teamnavn" className="input input-bordered input-info w-96 max-w-xs" />
           <select className="select select-info w-full max-w-xs">
             <option disabled selected>Distrikt</option>
             <option>Trondheim</option>
@@ -24,9 +28,14 @@ const OpprettTeam = (): JSX.Element => {
             <option>Bergen</option>
           </select>
           <input type="text" name="epost" placeholder="E-post" className="input input-bordered input-info w-full max-w-xs" />
-          <input placeholder="Beskrivelse" className="input input-bordered input-info w-full max-w-xs" />
-          <textarea placeholder="Beskrivelse" className="input input-bordered input-info w-full max-w-xs" />
-
+          <input placeholder="Beskrivelse" className="input input-bordered input-info max-w-xs" />
+          <input
+        name="bold"
+        type="checkbox"
+        checked={bold}
+        onChange={handleBoldChange}
+      />
+          <textarea placeholder="Beskrivelse" style={{ fontWeight: bold ? 'bold' : 'normal' }} className="textarea textarea-info"/>
           <button type="button" className="login-buttons bg-blue-400 hover:bg-blue-900 text-white px-4 py-2 rounded-full duration-300">
             Opprett team
           </button>
