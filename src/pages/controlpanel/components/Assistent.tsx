46f4b73afda9c8056a114a2bc5252326e8ed75d3
Filip Skaug <filis@stud.ntnu.no>
2024-01-24 17:59:07 +0100

    vf-101 add map-to-table for assistent nettside

--- src/pages/controlpanel/components/Assistent.tsx
+++ src/pages/controlpanel/components/Assistent.tsx
@@ -4,66 +4,97 @@ import Navbar from "./Navbar";
 const Assistent = (): JSX.Element => {
   const select = ["Assistenter", "Vikarer"]
   const [selected, setSelected] = useState<string>(select[0]);
-  // const mapToTable = (skoler: School[]) => {
-  //   return skoler.map((school, index) => (
-  //     <tr key={index.valueOf()} className="even:bg-white odd:bg-table-grey">
-  //       <td className="text-center py-3 px-6">{school.name}</td>
-  //       <td className="text-center py-3 px-6">{school.contactPerson}</td>
-  //       <td className="text-center py-3 px-6">{school.email}</td>
-  //       <td className="text-center py-3 px-6">{school.phone}</td>
-  //       <td className="text-center py-3 px-6">{school.language}</td>
-  //     </tr>
-  //   ));
-  // };
+
+  const mapToTable = (skoler: School[]) => {
+    return skoler.map((school, index) => (
+      <tr key={index.valueOf()} className="even:bg-white odd:bg-table-grey">
+        <td className="text-center py-3 px-6">{school.name}</td>
+        <td className="text-center py-3 px-6">{school.school}</td>
+        <td className="text-center py-3 px-6">{school.email}</td>
+        <td className="text-center py-3 px-6">{school.semester}</td>
+        <td className="text-center py-3 px-6">{school.department}</td>
+        <td className="text-center py-3 px-6">{school.bolk}</td>
+        <td className="text-center py-3 px-6">{school.dag}</td>
+      </tr>
+    ));
+  };
 
   interface School {
     name: string;
-    contactPerson: string;
+    school: string;
     email: string;
-    phone: string;
-    language: string;
+    semester: string;
+    department: string;
+    bolk: string;
+    dag: string;
   }
 
   const aktiveSkolerListe: School[] = [
     {
-      name: "Amalie Skram",
-      contactPerson: "Aaryan",
-      email: "Aaryan.er.kul@hotmail.com",
-      phone: "12345678",
-      language: "Arabisk",
-    },
-    {
-      name: "Blussuvoll",
-      contactPerson: "Maurice",
-      email: "email@domene.com",
-      phone: "12345678",
-      language: "Norsk",
-    },
-    {
-      name: "Charlottenlund",
-      contactPerson: "Ola",
-      email: "email@email.com",
-      phone: "12345678",
-      language: "Norsk",
+      name: "Ole Normann",
+      school: "BI",
+      email: "ole.normann@gmail.com",
+      semester: "Høst 2021",
+      department: "IT",
+      bolk: "Bolk 1, Bolk 2",
+      dag: "Mandag"
     },
   ];
 
-  const inaktiveSkolerListe: School[] = [
-    {
-      name: "St. Olav VGS",
-      contactPerson: "Ola Nordmann",
-      email: "testmail@gmail.com",
-      phone: "87654321",
-      language: "Nynorsk",
-    },
-  ];
+  // const inaktiveSkolerListe: School[] = [
+  //   {
+  //     name: "St. Olav VGS",
+  //     contactPerson: "Ola Nordmann",
+  //     email: "testmail@gmail.com",
+  //     phone: "87654321",
+  //     language: "Nynorsk",
+  //   },
+  // ];
 
   return (
     <div className="w-full">
       <div className="pt-10 pr-10 pl-10 mt-50 shadow grid-rows-2 grid-cols-2 flex flex-col items-center">
         <h1 className="text-2xl row-start-1 row-end-1">Assistenter</h1>
-        <div className="flex flex-wrap ml-16">
+        <div className="flex flex-wrap">
           <Navbar select={select} selected={selected} setSelected={setSelected} />
+        </div>
+      </div>
+      <div className="p-10 flex justify-center items-center ml-6">
+        <div className="w-10/12 border-2 mt-100 shadow overflow-x-scroll">
+          <table className="w-full">
+            <thead>
+              <tr className="bg-white">
+                <th className="text-center w-1/7 py-3 px-6 text-vektor-darblue">
+                  Navn
+                </th>
+                <th className="text-center w-1/7 py-3 px-6 text-vektor-darblue">
+                  Skole
+                </th>
+                <th className="text-center w-1/7 py-3 px-6 text-vektor-darblue">
+                  E-post
+                </th>
+                <th className="text-center w-1/7 py-3 px-6 text-vektor-darblue">
+                  Semester
+                </th>
+                <th className="text-center w-1/7 py-3 px-6 text-vektor-darblue">
+                  Avdeling
+                </th>
+                <th className="text-center w-1/7 py-3 px-6 text-vektor-darblue">
+                  Bolk
+                </th>
+                <th className="text-center w-1/7 py-3 px-6 text-vektor-darblue">
+                  Dag
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {mapToTable(
+                (selected === select[0]
+                  && aktiveSkolerListe)
+                  || ([]),
+              )}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
