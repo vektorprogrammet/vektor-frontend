23b240ec2b97bddcaecfed68fbd28c6e6226d1fc
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2024-01-31 19:38:33 +0100

    VF-111 finish opprett team

--- src/pages/controlpanel/components/OpprettTeam.tsx
+++ src/pages/controlpanel/components/OpprettTeam.tsx
@@ -1,60 +1,54 @@
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 import ReactQuill, { Quill } from 'react-quill';
-import ImageUploader from 'quill-image-uploader';
-
 import 'react-quill/dist/quill.snow.css';
+
 const OpprettTeam = (): JSX.Element => {
 
   const [value, setValue] = useState('');
-  Quill.register('modules/imageUploader', ImageUploader);
-
-  const formats = [
-    "header",
-    "bold",
-    "italic",
-    "underline",
-    "bullet",
-    "indent",
-    "link",
-    "image",
-  ];
 
   var toolbarOptions = [
-  [{ 'header': [1, 2, false] }],
+    [{ 'header': [1, 2, false] }],
 
-  ['bold', 'italic', 'underline', 'image'],        // toggled buttons
+    ['bold', 'italic', 'underline', 'image'],
 
-  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ 'indent': '-1' }, { 'indent': '+1' }],
 
-  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'color': [] }, { 'background': [] }],
 
-  ['clean']                                         // remove formatting button
-];
+    ['clean']                       
+  ];
 
   return (
     <div className="flex flex-col items-center space-between w-full">
       <h1 className="text-2xl row-start-1 row-end-1 pt-10">Opprett team</h1>
-      <div className="mt-32 flex text-2xl">
-        <div className="flex flex-col gap-12 text-xl">
-          <label>Navn:</label>
+      <div className="mt-32 flex text-2xl w-2/3">
+        <div className="flex flex-col gap-12 text-xl w-1/3">
+          <label >Navn:</label>
           <label>Distrikt:</label>
           <label>E-post:</label>
-          <label>Kort beskrivelse:</label>
+          <label >Kort beskrivelse:</label>
           <label>Lang beskrivelse:</label>
         </div>
-        <div className="ml-10 flex flex-col gap-6">
+        <div className="flex flex-col gap-6">
           <input type="text" placeholder="Teamnavn" className="input input-bordered input-info w-96 max-w-xs" />
-          <select className="select select-info w-full max-w-xs">
-            <option disabled selected>Distrikt</option>
+          <select className="select select-info w-full max-w-xs fit-content">
             <option>Trondheim</option>
             <option>Ås</option>
             <option>Bergen</option>
           </select>
           <input type="text" name="epost" placeholder="E-post" className="input input-bordered input-info w-full max-w-xs" />
           <input placeholder="Beskrivelse" className="input input-bordered input-info max-w-xs" />
-          <div className="textarea textarea-info h-64">
-            <ReactQuill theme="snow" value={value} modules={{toolbar: toolbarOptions}} onChange={setValue} className="h-48"/>
+          <div className="textarea textarea-info h-72 w-full">
+            <ReactQuill
+              theme="snow"
+              value={value}
+              modules={{
+                toolbar: toolbarOptions
+              }}
+              onChange={setValue}
+              className="h-48"
+            />
           </div>
           <button type="button" className="login-buttons bg-blue-400 hover:bg-blue-900 text-white px-4 py-2 rounded-full duration-300">
             Opprett team
