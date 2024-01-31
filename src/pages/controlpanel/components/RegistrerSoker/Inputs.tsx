2557ef45ea2e27680b994d9d0a1d4cbbefa00e68
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2024-01-31 18:08:50 +0100

    Fixed patterns.

--- src/pages/controlpanel/components/RegistrerSoker/Inputs.tsx
+++ src/pages/controlpanel/components/RegistrerSoker/Inputs.tsx
@@ -11,7 +11,6 @@ interface InputProps {
     label?: string,
     placeholder?: string,
     pattern?: string,
-    required?: boolean,
 }
 const toId = (str: string):string => {
     return "input-" + str;
@@ -24,10 +23,10 @@ const Input:React.FC<InputProps> = ({name, label, placeholder, pattern, type, er
                 <label className="" htmlFor={toId(name)}>{label}</label>
             }
             { error !== undefined &&
-                <input className="w-full" name={name} id={toId(name)} type={type} placeholder={placeholder} pattern={pattern} title={error} />
+                <input className="w-full" name={name} id={toId(name)} type={type} placeholder={placeholder} pattern={pattern} title={error} required/>
             }
             { error === undefined &&
-                <input className="w-full" name={name} id={toId(name)} type={type} placeholder={placeholder} pattern={pattern} />
+                <input className="w-full" name={name} id={toId(name)} type={type} placeholder={placeholder} pattern={pattern} required/>
             }
         </div>
     </>);
@@ -35,7 +34,7 @@ const Input:React.FC<InputProps> = ({name, label, placeholder, pattern, type, er
 
 export const NameInput:React.FC<TextProps> = ({ name, label }) => {
     return (<>
-        <Input name={name} type="text" label={label} error="Navnet må starte med stor bokstav, og kun ha gyldige bokstaver i seg."/>
+        <Input name={name} type="text" label={label} error="Navnet kan kun ha gy."/>
     </>);
 }
 interface HasName {
@@ -43,7 +42,7 @@ interface HasName {
 }
 export const PhoneInput:React.FC<HasName> = ({name}) => {
     return (<>
-        <Input name={name} label="Telefon" placeholder="--------" type="tel" error="Ikke et gyldig telefonnummer."/>
+        <Input name={name} label="Telefon" placeholder="--------" type="tel" pattern="\d{8}" error="Telefonnummeret må ha 8 siffer."/>
     </>)
 }
 
@@ -67,7 +66,7 @@ export const SelectInput:React.FC<SelectProps> = ({name, label, options}) => {
     return (<>
         <div className="w-5/6">
             <label className="" htmlFor={toId(name)}>{label}</label>
-            <select name={name} className="w-5/6 mr-4" id={toId(name)}>
+            <select name={name} className="w-5/6 mr-4" id={toId(name)} required>
                 <option value="" disabled selected>Velg</option>
                 {options.map((option) => (
                     <option value={option.id}>{option.name}</option>
