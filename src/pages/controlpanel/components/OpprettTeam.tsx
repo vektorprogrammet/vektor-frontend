5cb300f35f7c4633c72344bedd41dba93e8747c7
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2024-02-14 17:58:57 +0100

    VF-111 fix design

--- src/pages/controlpanel/components/OpprettTeam.tsx
+++ src/pages/controlpanel/components/OpprettTeam.tsx
@@ -8,41 +8,54 @@ const OpprettTeam = (): JSX.Element => {
 
   var toolbarOptions = [
     [{ 'header': [1, 2, false] }],
-    [{ 'size': [ 'small', 'large'] }],
+    [{ 'size': ['small', 'large'] }],
 
     ['bold', 'italic', 'underline', 'image'],
 
     [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-    [{ 'indent': '-1' }, { 'indent': '+1' }],                      
+    [{ 'indent': '-1' }, { 'indent': '+1' }],
   ];
 
   return (
     <div className="flex flex-col items-center space-between w-full ml-24 mr-2">
       <h1 className="text-2xl row-start-1 row-end-1 pt-6">Opprett team</h1>
-      <div className="mt-12 flex text-xl">
-        <div className="flex flex-col gap-6 items-center">
-          <input type="text" placeholder="Teamnavn" className="input input-bordered input-info w-72 max-w-72" />
-          <select className="select select-info w-72 max-w-72 fit-content">
-            <option>Trondheim</option>
-            <option>Ås</option>
-            <option>Bergen</option>
-          </select>
-          <input type="text" name="epost" placeholder="E-post" className="input input-bordered input-info w-72 max-w-72" />
-          <input placeholder="Beskrivelse" className="input input-bordered input-info w-72 max-w-72" />
-          <label>Lang beskrivelse:</label>
-          <div className="textarea textarea-info h-72 w-full max-w-xl">
-            <ReactQuill
-              theme="snow"
-              value={value}
-              modules={{
-                toolbar: toolbarOptions
-              }}
-              onChange={setValue}
-              className="h-48"
-            />
+      <div className="mt-12 flex-col items-center">
+        <div className="flex flex-wrap justify-center gap-2">
+          <div className="flex flex-col">
+            <label className="label" htmlFor="teamnavn">Teamnavn</label>
+            <input type="text" className="input input-bordered input-info w-72 min-w-72 min-h-12" id="teamnavn" />
+            <label className="label" htmlFor="email">Email</label>
+            <label className="input input-bordered input-info flex items-center gap-2 w-72 min-h-12">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+              <input type="text" className="grow" id="email" />
+            </label>
           </div>
-          <button type="button" className="login-buttons bg-blue-400 hover:bg-blue-900 text-white px-4 py-2 rounded-full duration-300">
-            Opprett team
+          <div className="flex flex-col">
+            <label className="label" htmlFor="region">Region</label>
+            <select className="select select-info w-72 max-w-72 fit-content" id="region">
+              <option>Trondheim</option>
+              <option>Ås</option>
+              <option>Bergen</option>
+            </select>
+            <label className="label" htmlFor="beskrivelse">Beskrivelse</label>
+            <input className="input input-bordered input-info w-72 min-h-12" id="beskrivelse" />
+          </div>
+        </div>
+        <label className="label mt-8" htmlFor="lang-beskrivelse">Lang beskrivelse</label>
+        <div className="textarea textarea-info h-72 w-full max-w-xl" id="lang-beskrivelse">
+          <ReactQuill
+            theme="snow"
+            value={value}
+            modules={{
+              toolbar: toolbarOptions
+            }}
+            onChange={setValue}
+            className="h-48"
+          />
+        </div>
+        <div className="w-full flex justify-center">
+          <button type="button" className="login-buttons bg-blue-400 hover:bg-blue-900 text-white px-4 py-2 mt-12 rounded-full duration-300 ">
+          Opprett team
           </button>
         </div>
       </div>
