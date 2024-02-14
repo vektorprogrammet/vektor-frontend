d50bd38b695f5592c25deaac41819e33089ffc8c
kyrkjedelen <44161717+kyrkjedelen@users.noreply.github.com>
2024-02-14 18:40:43 +0100

    Vf 106 lag registrer søker side (#262)
    * Added route to register.
    
    * Added components, without tailwind
    
    * Added styling to the registrer søker component
    
    * Fixed general inputs, and added them to the page.
    
    * Finalised styling.
    
    * Finalised styling.
    
    * Finalised styling.(2)
    
    * Fixed pattern and placeholder
    
    * Fixed patterns.
    
    * Added media-queries
    
    * Fixed consistency with opprett-team siden
    
    * finpuss
    
    * lint fixing, både manuelt og automatisk
    
    ---------
    
    Co-authored-by: aaryanneupane <aaryan200513@gmail.com>

--- src/pages/controlpanel/routes.tsx
+++ src/pages/controlpanel/routes.tsx
@@ -3,6 +3,7 @@ import Kontrollpanel from "pages/controlpanel/components/Kontrollpanel";
 import { AppRoute } from "App";
 import Skoler from "pages/controlpanel/components/Skoler";
 import Teaminteresse from "pages/controlpanel/components/Teaminteresse";
+import RegistrerSoker from "./components/RegistrerSoker/RegistrerSoker";
 import Teammedlemmer from "./components/Teammedlemmer";
 
 // The route with the corresponding component to render in the route
@@ -11,6 +12,7 @@ const routes: AppRoute[] = [
   { path: "/kontrollpanel/skoler", element: <Skoler />, name: "Skoler" },
   { path: "/kontrollpanel/teams/:id", element: <Teammedlemmer />, name: "Teammedlemmer" },
   { path: "/kontrollpanel/teaminteresse", element: <Teaminteresse />, name: "Teaminteresse" },
+  { path: "/kontrollpanel/registrer-soker", element: <RegistrerSoker />, name: "Registrer søker" },
 ];
 
 export default routes;
