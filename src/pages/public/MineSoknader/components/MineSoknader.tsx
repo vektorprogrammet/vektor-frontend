5748cfd6b745e596d3c1d90badc47c76ee19211c
mauricew <mauricew@stud.ntnu.no>
2024-02-21 17:01:58 +0100

    Lag søknaderkomponent

--- src/pages/public/MineSoknader/components/MineSoknader.tsx
+++ src/pages/public/MineSoknader/components/MineSoknader.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProfileModal from "./ProfileModal";
+import Applications from "./Applications";
 
 const MineSoknader = (): JSX.Element => {
   return (
@@ -11,6 +12,26 @@ const MineSoknader = (): JSX.Element => {
         <ProfileModal
           imgUrl="https://vektorprogrammet.no/media/cache/profile_img/images/Profile%20photos/644805d1e8ef2.jpeg"
           name="Aaryan Neupan"
+        />
+        <Applications
+          applications={[
+            {
+              role: "IT-leder Høst 2024",
+              status: "Avslått",
+              expectedAction:
+                "Ingen videre handling er nødvendig. Du vil ikke bli leder for IT høsten 2024.",
+            },
+            {
+              role: "Vektorassistent Høst 2023",
+              status: "Under vurdering",
+              expectedAction: "Vente på svar",
+            },
+            {
+              role: "Vektorassistent Vår 2022",
+              status: "Innvilget",
+              expectedAction: "Vente på mail med videre informasjon",
+            },
+          ]}
         />
       </div>
     </div>
