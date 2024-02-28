e4cd9dc1f66b0f2fb48ff6dfaf7f7e926c82296c
phibkro <philib.krogh@gmail.com>
2024-02-28 22:35:20 +0100

    feat: :sparkles: add NavHeader with working searchParams

--- src/pages/controlpanel/components/NavHeader.tsx
+++ src/pages/controlpanel/components/NavHeader.tsx
@@ -1,51 +1,98 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
-function capitalizeFirstLetter(string: string): string {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-const Breadcrumbs = (): JSX.Element => {
-  const { pathname } = useLocation();
-  const paths = pathname.split("/").filter((path) => path !== "");
+import { useSearchParams } from "react-router-dom";
+import Breadcrumbs from "./Breadcrumbs";
+
+const validCities = {
+  bergen: "Bergen",
+  trondheim: "Trondheim",
+  ås: "Ås",
+} as const;
+const semesters = {
+  15: "Vår 2015",
+  16: "Høst 2015",
+  17: "Vår 2016",
+  18: "Høst 2016",
+  19: "Vår 2017",
+  20: "Høst 2017",
+  21: "Vår 2018",
+  22: "Høst 2018",
+  23: "Vår 2019",
+  24: "Høst 2019",
+  93: "Vår 2020",
+  94: "Høst 2020",
+};
+
+type SearchParams = {
+  semester: string;
+  city: string;
+};
+const SearchParamsForm = (): JSX.Element => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleChange = (data: SearchParams) => {
+    setSearchParams(data);
+  };
+
+  const searchParamsSemester = searchParams.get("semester");
+  const searchParamsCity = searchParams.get("city");
+
   return (
-    <div className="flex gap-2 breadcrumbs">
-      <ul>
-        <li>
-          <Link
-            to="/"
-            className="text-vektor-blue hover:text-vektor-blue-hover"
-          >
-            Forsiden
-          </Link>
-        </li>
-        {paths.map((path, index) => {
-          if (index === paths.length - 1) {
-            return (
-              <li>
-                <span>{capitalizeFirstLetter(path)}</span>
-              </li>
-            );
-          }
+    <form className="flex items-center gap-2 mx-4 my-2">
+      <select
+        id="semester"
+        className="select select-bordered z-[1] menu shadow bg-base-100 rounded-box"
+        onChange={(e) => {
+          handleChange({
+            semester: e.target.value,
+            city: searchParams.get("city") || "",
+          });
+        }}
+      >
+        <option disabled defaultValue={searchParamsSemester || "Velg semester"}>
+          {searchParamsSemester || "Velg semester"}
+        </option>
+        {Object.entries(semesters).map((semester) => {
+          const semesterName = semester[1];
           return (
-            <li>
-              <Link
-                to={path}
-                key={index}
-                className="text-vektor-blue hover:text-vektor-blue-hover"
-              >
-                {capitalizeFirstLetter(path)}
-              </Link>
-            </li>
+            <option key={crypto.randomUUID()} value={semesterName}>
+              {semesterName}
+            </option>
           );
         })}
-      </ul>
-    </div>
+      </select>
+
+      <select
+        id="city"
+        className="select select-bordered z-[1] menu shadow bg-base-100 rounded-box"
+        onChange={(e) => {
+          handleChange({
+            semester: searchParamsSemester || "",
+            city: e.target.value,
+          });
+        }}
+      >
+        <option disabled defaultValue={searchParamsCity || "Velg by"}>
+          {searchParamsCity || "Velg by"}
+        </option>
+        {Object.values(validCities).map((city) => {
+          return (
+            <option key={crypto.randomUUID()} value={city}>
+              {city}
+            </option>
+          );
+        })}
+      </select>
+    </form>
   );
 };
+
 const NavHeader = (): JSX.Element => {
   return (
-    <div className="flex gap-2">
+    <nav className="flex gap-2 justify-between bg-blue-200">
       <Breadcrumbs />
-    </div>
+
+      <SearchParamsForm />
+    </nav>
   );
 };
 
