a6dd28a808128fa6fac51e50d6d2844d6cbc526e
mauricew <mauricew@stud.ntnu.no>
2024-04-09 17:55:39 +0200

    Create teams table in controlpanel

---
+++ src/pages/controlpanel/components/Team.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import CreateTable from "./CreateTable";
+
+const Team = (): JSX.Element => {
+  interface Team {
+    name: string;
+    email: string;
+    opptak: string;
+  }
+
+  const example1: Team[] = [
+    {
+      name: "IT",
+      email: "Aaryan.er.kul@hotmail.com",
+      opptak: "Åpent",
+    },
+    {
+      name: "Evaluering",
+      email: "email@domene.com",
+      opptak: "Stengt",
+    },
+    {
+      name: "Rekruttering",
+      email: "email@email.com",
+      opptak: "Stengt",
+    },
+    {
+      name: "Proffilering",
+      email: "email@email.com",
+      opptak: "Stengt",
+    },
+    {
+      name: "Skolekoordinering",
+      email: "email@email.com",
+      opptak: "Stengt",
+    },
+    {
+      name: "Sponsor",
+      email: "email@email.com",
+      opptak: "Stengt",
+    },
+    {
+      name: "Styret",
+      email: "email@email.com",
+      opptak: "Stengt",
+    },
+    {
+      name: "Økonomi",
+      email: "email@email.com",
+      opptak: "Stengt",
+    },
+  ];
+
+  return (
+    <div className="w-full">
+      <div className="pt-10 mt-50 shadow grid-rows-2 grid-cols-2 flex flex-col items-center">
+        <h1 className="text-2xl row-start-1 row-end-1">Team</h1>
+        <CreateTable header={["Navn", "Epost", "Opptak"]} content={example1} />
+      </div>
+    </div>
+  );
+};
+
+export default Team;
--- src/pages/controlpanel/routes.tsx
+++ src/pages/controlpanel/routes.tsx
@@ -6,15 +6,33 @@ import Teaminteresse from "pages/controlpanel/components/Teaminteresse";
 import Assistent from "pages/controlpanel/components/Assistent";
 import RegistrerSoker from "./components/RegistrerSoker/RegistrerSoker";
 import Teammedlemmer from "./components/Teammedlemmer";
+import Team from "./components/Team";
 
 // The route with the corresponding component to render in the route
 const routes: AppRoute[] = [
   { path: "", element: <Kontrollpanel />, name: "Kontrollpanel" },
+  { path: "/kontrollpanel/team", element: <Team />, name: "Team" },
   { path: "/kontrollpanel/skoler", element: <Skoler />, name: "Skoler" },
-  { path: "/kontrollpanel/teams/:id", element: <Teammedlemmer />, name: "Teammedlemmer" },
-  { path: "/kontrollpanel/teaminteresse", element: <Teaminteresse />, name: "Teaminteresse" },
-  { path: "/kontrollpanel/assistenter", element: <Assistent />, name: "Assistent" },
-  { path: "/kontrollpanel/registrer-soker", element: <RegistrerSoker />, name: "Registrer søker" },
+  {
+    path: "/kontrollpanel/teams/:id",
+    element: <Teammedlemmer />,
+    name: "Teammedlemmer",
+  },
+  {
+    path: "/kontrollpanel/teaminteresse",
+    element: <Teaminteresse />,
+    name: "Teaminteresse",
+  },
+  {
+    path: "/kontrollpanel/assistenter",
+    element: <Assistent />,
+    name: "Assistent",
+  },
+  {
+    path: "/kontrollpanel/registrer-soker",
+    element: <RegistrerSoker />,
+    name: "Registrer søker",
+  },
 ];
 
 export default routes;
