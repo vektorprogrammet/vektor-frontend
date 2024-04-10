3221ce35b67f902d42a276e147cfb8a2e1b59f76
maurice-wegerif <102590927+maurice-wegerif@users.noreply.github.com>
2024-04-10 17:56:24 +0200

    VF-113 Create teams table in controlpanel (#271)
    * Create teams table in controlpanel
    
    * Test new email
    
    * Legg til opprett team knapp
    
    * Fikse lint error
    
    ---------
    
    Co-authored-by: mauricew <mauricew@stud.ntnu.no>

--- src/pages/controlpanel/routes.tsx
+++ src/pages/controlpanel/routes.tsx
@@ -5,16 +5,34 @@ import Skoler from "pages/controlpanel/components/Skoler";
 import Teaminteresse from "pages/controlpanel/components/Teaminteresse";
 import Assistent from "pages/controlpanel/components/Assistent";
 import RegistrerSoker from "./components/RegistrerSoker/RegistrerSoker";
-import Teammedlemmer from "./components/Teammedlemmer";
+import Teammedlemmer from "./components/Team/Teammedlemmer";
+import Team from "./components/Team/Team";
 
 // The route with the corresponding component to render in the route
 const routes: AppRoute[] = [
   { path: "", element: <Kontrollpanel />, name: "Kontrollpanel" },
+  { path: "/kontrollpanel/team", element: <Team />, name: "Team" },
   { path: "/kontrollpanel/skoler", element: <Skoler />, name: "Skoler" },
-  { path: "/kontrollpanel/teams/:id", element: <Teammedlemmer />, name: "Teammedlemmer" },
-  { path: "/kontrollpanel/teaminteresse", element: <Teaminteresse />, name: "Teaminteresse" },
-  { path: "/kontrollpanel/assistenter", element: <Assistent />, name: "Assistent" },
-  { path: "/kontrollpanel/registrer-soker", element: <RegistrerSoker />, name: "Registrer søker" },
+  {
+    path: "/kontrollpanel/teams/:id",
+    element: <Teammedlemmer />,
+    name: "Teammedlemmer",
+  },
+  {
+    path: "/kontrollpanel/teaminteresse",
+    element: <Teaminteresse />,
+    name: "Teaminteresse",
+  },
+  {
+    path: "/kontrollpanel/assistenter",
+    element: <Assistent />,
+    name: "Assistent",
+  },
+  {
+    path: "/kontrollpanel/registrer-soker",
+    element: <RegistrerSoker />,
+    name: "Registrer søker",
+  },
 ];
 
 export default routes;
