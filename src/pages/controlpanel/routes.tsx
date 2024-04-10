4772a1177f6c148cba4afa4b9db7348e53081375
olejacobmellgren <115150917+olejacobmellgren@users.noreply.github.com>
2024-04-10 18:05:58 +0200

    Vf 111 lag opprett team side (#261)
    * vf-111 opprett team side
    
    * VF-111 fix style css
    
    Fix styling css
    
    * VF-111 add select distrikt
    
    * VF-111 change route to avoid bug
    
    * VF-111 Minor fixes opprettTeam side
    
    * VF-111 Legge til text editor
    
    * VF-111 delete yarn.lock
    
    * VF-111 fikse beskrivelse tekstboks
    
    * VF-111 finish opprett team
    
    * VF-111 Lag opprett-team responsiv
    
    * VF-111 fix design
    
    * VF-111 run lint
    
    * VF-111 comments in utlegg
    
    * VF-111 lint fix
    
    * lint fix
    
    * VF-111 remove yarn
    
    * vf-111 yarn.lock installed

--- src/pages/controlpanel/routes.tsx
+++ src/pages/controlpanel/routes.tsx
@@ -4,6 +4,7 @@ import { AppRoute } from "App";
 import Skoler from "pages/controlpanel/components/Skoler";
 import Teaminteresse from "pages/controlpanel/components/Teaminteresse";
 import Assistent from "pages/controlpanel/components/Assistent";
+import OpprettTeam from "./components/Team/OpprettTeam";
 import RegistrerSoker from "./components/RegistrerSoker/RegistrerSoker";
 import Teammedlemmer from "./components/Team/Teammedlemmer";
 import Team from "./components/Team/Team";
@@ -13,6 +14,7 @@ const routes: AppRoute[] = [
   { path: "", element: <Kontrollpanel />, name: "Kontrollpanel" },
   { path: "/kontrollpanel/team", element: <Team />, name: "Team" },
   { path: "/kontrollpanel/skoler", element: <Skoler />, name: "Skoler" },
+  { path: "/kontrollpanel/opprett-team", element: <OpprettTeam />, name: "Opprett nytt team" },
   {
     path: "/kontrollpanel/teams/:id",
     element: <Teammedlemmer />,
