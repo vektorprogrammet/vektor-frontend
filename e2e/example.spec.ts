a6b6f2d3ad4705a2f6490cf84ea505030eb40672
Julia Dai <juliada@stud.ntnu.no>
2024-04-23 17:24:25 +0200

    Update example test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,20 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-/* test.beforeEach( async ({ page }) => {
-  await page.goto('/');
-  // Expect a title "to contain" a substring.
-  //await expect(page).toHaveTitle('Vektorprogrammet');
-});
+
 
 test('has title', async ({page}) => {
+  await page.goto('/');
   await expect(page).toHaveTitle('Vektorprogrammet');
-}) */
+})
 
 test.describe('Navbar', () => {
   test.beforeEach( async ({ page }) => {
     await page.goto('/');
-    // Expect a title "to contain" a substring.
-    //await expect(page).toHaveTitle('Vektorprogrammet');
   });
 
   test('can navigate to assistent page', async ({page}) => {
@@ -38,40 +33,85 @@ test.describe('Navbar', () => {
   })
 })
 
-/* test.describe('Navbar', () => {
-  test('can navigate to different pages through navbar', async ({page}) => {
-    await page.goto('/');
-    await page.getByRole('link', { name: 'Assistenter' }).click();
-    await expect(page.getByRole('heading', { name: 'Assistenter' })).toBeVisible();
-
-    await page.getByRole('link', { name: 'Foreldre' }).click();
-    await expect(page.getByRole('heading', { name: 'Informasjon for foreldre' })).toBeVisible();
-
-    await page.getByRole('link', { name: 'Team' }).click();
-    await expect(page.getByRole('heading', { name: 'Styre og team' })).toBeVisible();
-    
-    await page.getByRole('link', { name: 'Skoler' }).click();
-    await expect(page.getByRole('heading', { name: 'Vektorassistenter i skolen' })).toBeVisible();
+test.describe('Contact page', () => {
+  test.beforeEach( async ({ page }) => {
+    await page.goto('/kontakt');
   })
-}) */
+
+  test('has contact informations for Trondheim and contact', async ({ page }) => {
+    await page.locator('button').filter({ hasText: 'Trondheim' }).click();
+    await expect(
+      page.locator('div').filter({ hasText: /^styret\.ntnu@vektorprogrammet\.no$/ }).getByRole('link'))
+      .toBeVisible();
+    await expect(page.getByText('Høgskoleringen 5 7491')).toBeVisible();
+  })
+
+  test('can submit message in contact form', async ({ page }) => {
+    const name = page.getByLabel('Ditt navn');
+    const email = page.getByLabel('Din e-post');
+    const subject = page.getByLabel('Emne');
+    const message = page.getByLabel('Melding');
+    const submitButton = page.getByRole('button', { name: 'Send melding' });
+
+    await expect(name).toBeVisible();
+    await expect(email).toBeVisible();
+    await expect(subject).toBeVisible();
+    await expect(message).toBeVisible();
+    await expect(submitButton).toBeVisible();
+
+    await name.fill('John Doe');
+    await email.fill('johndoe@gmail.com');
+    await subject.fill('teams');
+    await message.fill('How do I apply for teams?');
+
+    await expect(name).not.toBeEmpty();
+    await expect(email).not.toBeEmpty();
+    await expect(subject).not.toBeEmpty();
+    await expect(message).not.toBeEmpty();
+
+    submitButton.click();
+    await expect(name).toBeEmpty();
+    await expect(email).toBeEmpty();
+    await expect(subject).toBeEmpty();
+    await expect(message).toBeEmpty();
+  })
+  
+})
 
 test.describe('Assistent page', () => {
   test.beforeEach( async ({ page }) => {
     await page.goto('/assistenter');
   });
 
-  test('can choose city', async ({page}) => {
-    //await expect(page.getByRole('tablist', { name: bergen/i/ })).toBeVisible();
-    const button = await page.getByRole('button', { name: /bergen/i });
-    //await expect(button).toBeVisible();
-    await button.click();
+  test('can use button to scroll down to application form', async ({page}) => {
+    const scrollButton = await page.getByRole('button', { name: 'Scroll ned for å søke!' });
+    scrollButton.click();
+    await expect(page.getByRole('button', { name: 'Søk nå!' })).toBeInViewport();
+  })
+
+  test('can change city', async ({page}) => {
+    const trondheimButton = await page.locator('button').filter({ hasText: 'Trondheim' })
+    const bergenButton = await page.getByRole('main').getByText('Bergen');
+    const aasButton = await page.getByRole('main').getByText('Ås');
+
+    await expect(trondheimButton).toBeVisible();
+    await expect(bergenButton).toBeVisible();
+    await expect(aasButton).toBeVisible();
+
+    await trondheimButton.click();
+    await expect(page.getByRole('heading', { name: 'Trondheim' })).toBeVisible();
+
+    await bergenButton.click();
+    await expect(page.getByRole('heading', { name: 'Bergen' })).toBeVisible();
+
+    await aasButton.click();
+    await expect(page.getByRole('heading', { name: 'Ås' })).toBeVisible();
+
   })
 
   test('can submit application', async ({page}) => {
-    //await page.goto('/assistenter');
-
     await expect(page.getByRole('heading', { name: 'Trondheim' })).toBeVisible();
-    const submit = page.getByRole('button', { name: /søk nå!/i });
+    const submit = page.getByRole('button', { name: 'Søk nå!' });
 
     const firstname = page.getByPlaceholder('Fornavn');
     const surname = page.getByPlaceholder('Etternavn');
@@ -93,7 +133,7 @@ test.describe('Assistent page', () => {
     await studyprogram.fill('mtdt');
 
     await submit.click();
-
+    await expect(page.getByRole('heading', { name: 'Assistenter' })).toBeInViewport()
   })
   
 
