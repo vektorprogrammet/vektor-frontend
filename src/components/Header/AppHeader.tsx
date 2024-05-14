c7cc2da0dffc28cd27f13afca105c421d8c6e6ef
Filip Skaug <filis@stud.ntnu.no>
2024-05-14 20:33:02 +0200

    app avatar dropdown style

--- src/components/Header/AppHeader.tsx
+++ src/components/Header/AppHeader.tsx
@@ -9,17 +9,16 @@ import UserAvatar from "./UserAvatar";
 
 const activeStyle: React.CSSProperties = {
   fontWeight: "bold",
-  color: "#023874",
 };
 
 const AppHeader = (): JSX.Element => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [loginPopupVisible, setLoginPopupVisible] = useState(false);
   const isLoggedIn = true;
-  const linkElements = routes.filter(route => route.name).map((route) => (
+  const linkElements = routes.map((route) => (
     <NavLink
       key={route.path}
-      className="md:text-gray-600 md:hover:text-gray-900 md:dark:text-white p-1 font-semibold text-nowrap"
+      className="md:text-gray-600 md:hover:text-blue-400 md:dark:text-white p-1 font-semibold text-nowrap"
       to={route.path ?? ""}
       style={({ isActive }) => (isActive ? activeStyle : {})}
     >
@@ -28,7 +27,7 @@ const AppHeader = (): JSX.Element => {
   ));
 
   return (
-    <nav className="md:sticky top-0  z-50 bg-[rgba(226,244,250,0.85)] backdrop-blur-sm">
+    <nav className="md:sticky top-0  z-50 bg-[rgba(226,244,250,0.9)] backdrop-blur-sm">
       <div className="hidden md:flex p-1 justify-between lg:px-8 w-full">
         <div className="w-fit justify-center flex">
           <Link to="/">
