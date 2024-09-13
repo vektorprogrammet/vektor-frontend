4ec8363d08e6d5920eef86657f3f7c53c0456028
Filip Skaug <filis@stud.ntnu.no>
2024-09-13 12:04:11 +0200

    vf-158 add profile icon to mobile view and change icon design

--- src/components/Header/MobileMenu.tsx
+++ src/components/Header/MobileMenu.tsx
@@ -1,5 +1,7 @@
-import React, { type Dispatch, type SetStateAction } from "react";
+import type React from "react";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { Link, NavLink } from "react-router-dom";
 import routes from "../../pages/public/routes";
 import LoginButtons from "./LoginButtons";
@@ -12,7 +14,7 @@ interface Props {
   setMenuOpen: (values: boolean) => void;
   isLoggedIn: boolean;
   loginPopupVisible: boolean;
-  setLoginPopupVisible: Dispatch<SetStateAction<boolean>>;
+  setLoginPopupVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const activeStyle: React.CSSProperties = {
@@ -41,14 +43,28 @@ const MobileMenu = (props: Props): JSX.Element => {
     </NavLink>
   ));
   return (
-    <button tabIndex={0} className="md:hidden fixed bottom-10 right-5 dropdown dropdown-top dropdown-end" type="button">
-      <div className="btn btn-lg bg-primary border-black border-2 rounded-[1.2rem] shadow-[inset_-5px_-8px_20px_-10px_rgba(0,0,0,0.5)]">
-        <p>=</p>
+    <div className="md:hidden">
+      <div className="fixed top-5 right-0 bg-[rgba(0,0,0,0.8)] p-1 pr-2 rounded-l-full" >
+        <div className="flex w-fit justify-center">
+          {isLoggedIn ? (
+            <UserAvatar />
+          ) : (
+            <LoginButtons setVisible={setLoginPopupVisible} />
+          )}
+          {loginPopupVisible ? (
+            <LoginPopup setVisible={setLoginPopupVisible} />
+          ) : null}
+        </div>
       </div>
-      <ul className="mb-2 p-0 overflow-hidden shadow menu menu-compact dropdown-content bg-[#EDF8FC] text-[#313131] rounded-xl w-fit">
-        {linkElements}
-      </ul>
-    </button>
+      <button tabIndex={0} className="fixed bottom-10 right-0 bg-[rgba(0,0,0,0.8)] p-1 pr-2 rounded-l-full dropdown dropdown-top dropdown-end" type="button">
+        <div className="btn btn-lg bg-primary min-h-16 min-w-16 p-0 border-black border-2 rounded-full shadow-[inset_-5px_-8px_20px_-10px_rgba(0,0,0,0.5)]">
+          <FontAwesomeIcon icon={faBars} className="fa-2xl" />
+        </div>
+        <ul className="mb-2 p-0 overflow-hidden shadow menu menu-compact dropdown-content bg-[#EDF8FC] text-[#313131] rounded-xl w-fit">
+          {linkElements}
+        </ul>
+      </button>
+    </div>
   );
 };
 
