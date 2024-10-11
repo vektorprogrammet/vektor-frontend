c9e8f3b162c5c0cba2b889ae1c5aa627220f80da
Julia Dai <juliada@stud.ntnu.no>
2024-10-11 11:18:07 +0200

    vf-166 update team division cards styling

--- src/pages/public/Team/components/Division.tsx
+++ src/pages/public/Team/components/Division.tsx
@@ -18,66 +18,49 @@ const Division = ({
   url,
 }: Props): JSX.Element => {
   const navigate = useNavigate();
+
+  const chosenStyle =
+          title === "Styret"
+          ? "w-64"
+          : "w-64";
+
   return (
-    <div className="relative h-full border-secondary content-center grow rounded bg-accent w-64 shadow-md border-2 place-items-center dark:bg-gray-600">
-      <div className="justify-between py-1 text-center border-b-2 border-secondary text-blue-900 text-lg font-bold dark:text-blue-300">
-        {title}
+    <div className={`h-48 flex flex-col justify-between rounded-md shadow-md bg-[#c7ecf8] dark:bg-gray-600 hover:scale-105 ${chosenStyle}`} onClick={() => navigate(`/team/${url}`)}>
+      <div className="bg-vektor-blue rounded-t-md h-10 content-center h-20">
+        <h1 className="text-center text-lg text-vektor-darblue font-medium">
+          {title}
+        </h1>
       </div>
-      <div className="text-sm mt-2 bg-accent p-2 rounded cursor-pointer hover:bg-grey-lighter pb-14 dark:bg-gray-600 dark:text-gray-300">
-        {text}
-        <div className="space-y-1 pt-12">
-          <div className="flex space-x-1 space-y-0 items-end">
-            <svg
-              className="h-4 w-4 text-balck"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" />
-              <rect x="3" y="5" width="18" height="14" rx="2" />
-              <polyline points="3 7 12 13 21 7" />
-            </svg>
-            <a
-              className="text-sm truncate overflow-hidden hover:underline"
-              href={`mailto:${mail}`}
-            >
-              {mail}
-            </a>
-          </div>
-          <div className="flex flex-row space-x-1 items-center">
-            <svg
-              className="h-4 w-4 text-black"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-              />
-            </svg>
-            <div className="top-0">{`${numberOfMembers} medlemmer`}</div>
-          </div>
-        </div>
+      <div className="text-sm mx-2 h-full my-2 mx-3">
+        <p>{text}</p>
       </div>
-      <div className="absolute inset-x-0 bottom-0 align-bottom">
-        <button
-          type="button"
-          onClick={() => navigate(`/team/${url}`)}
-          className="py-2 px-4 w-full font-medium text-mid border-b inset-x-0 bottom-0 bg-vektor-darblue hover:bg-vektor-blue text-white font duration-200"
+      <div className="flex flex-row text-sm space-x-1 content-end mx-3">
+        <svg
+          className="h-4 w-4 text-black"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+          />
+        </svg>
+        <div>{`${numberOfMembers} medlemmer`}</div>
+      </div>
+      <div className="flex w-full justify-end self-end mb-1.5 mr-1.5">
+        <button
+            type="button"
+            onClick={() => navigate(`/team/${url}`)}
+            className="btn btn-success btn-sm text-white rounded-full"
+          >
           {button_name}
         </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default Division;
