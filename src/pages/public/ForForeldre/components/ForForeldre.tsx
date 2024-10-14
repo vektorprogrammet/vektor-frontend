6c6eb5782e6571d7121f892416e6df63878239e0
Filip Skaug <filis@stud.ntnu.no>
2024-10-14 18:07:01 +0200

    unviersal colors forforeldre

--- src/pages/public/ForForeldre/components/ForForeldre.tsx
+++ src/pages/public/ForForeldre/components/ForForeldre.tsx
@@ -4,8 +4,8 @@ import TextPictureCard from "@/components/TextPictureCard";
 const ForForeldre = (): JSX.Element => {
   const { title, ingress, cards, bottomText } = getContent();
   return (
-    <div className="max-w-screen-lg mt-20 mb-20 mx-auto flex flex-col items-center dark:text-gray-300">
-      <h1 className="max-w-2xl text-gray-600 text-4xl text-center font-bold mx-3 dark:text-gray-200">
+    <div className="max-w-screen-lg mt-20 mb-20 mx-auto flex flex-col items-center dark:text-text-dark">
+      <h1 className="max-w-2xl text-vektor-DARKblue text-4xl text-center font-bold mx-3 dark:text-text-dark">
         {title}
       </h1>
       <p className="max-w-2xl text-center mt-4 mb-20 text-xl mx-3">{ingress}</p>
