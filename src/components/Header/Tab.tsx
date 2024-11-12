8f6873fc9f5edce4b461419b405aff15f0f3b569
Filip Skaug <filis@stud.ntnu.no>
2024-11-12 17:21:29 +0100

    vf-193 finishing tuches

--- src/components/Header/Tab.tsx
+++ src/components/Header/Tab.tsx
@@ -1,16 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 import routes from "@/pages/public/routes";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 
 export const Tab = () => {
   const tabsRef = useRef<(HTMLElement | null)[]>([]);
   const [tabUnderlineWidth, setTabUnderlineWidth] = useState(0);
   const [tabUnderlineLeft, setTabUnderlineLeft] = useState(0);
   const navigate = useNavigate();
-
-  const [activeTabIndex, setActiveTabIndex] = useState<number>(
-    () => Number(localStorage.getItem("activeTabIndex")) || 0
-  );
+  const [activeTabIndex, setActiveTabIndex] = useState<number>(0);
 
   useEffect(() => {
     if (activeTabIndex === null) {
