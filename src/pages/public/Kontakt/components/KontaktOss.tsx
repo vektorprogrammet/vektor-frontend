e3c9d5b95ce4b797a198af99f6686f5148772b76
Philip B. Krogh <71797726+phibkro@users.noreply.github.com>
2025-02-06 10:41:15 +0100

    Set up shadcn/ui (#418)
    * Revert "fix(deps): update dependency tailwindcss to v4 (#390)"
    
    This reverts commit de9caa94e129e489f675a3b82abf3ea2134c776c.
    
    * Revert "fix(deps): update react monorepo to v19 (major) (#383)"
    
    This reverts commit c92fa7f61dbc689fe370a1149d71410ff18044ca.
    
    * fix lock file
    
    * pnpm dlx shadcn@latest init
    
    style: default
    baseColor: slate
    
    * update shadcn drawer
    
    * remove redundant tw classes
    
    * remove unused component

--- src/pages/public/Kontakt/components/KontaktOss.tsx
+++ src/pages/public/Kontakt/components/KontaktOss.tsx
@@ -160,11 +160,11 @@ const KontaktTabs = ({ divisions }: KontaktTabProps): JSX.Element => {
     sessionStorage.setItem("kontaktTab", openTab.toString());
   }, [openTab]);
   return (
-    <div className="flex flex-col md:flex-row items-start md:max-w-6xl md:mb-auto mb-6 items-start">
+    <div className="flex flex-col md:flex-row items-start md:max-w-6xl md:mb-auto mb-6">
       <div className="w-1/5 ml-3">
         <Tabs divisions={divisions} tabstate={openTab} setOpenTab={setOpenTab}/>
       </div>
-      <div className="flex flex-col w-full items-start overflow-y-scroll h-[500px] break-words mb-6 rounded-md px-5 py-5 mx-auto xl:ml-20 lg:ml-16 w-[322px] sm:w-[440px] md:w-[720px] lg:w-[820px] xl:w-[1100px]">
+      <div className="flex flex-col w-full items-start overflow-y-scroll h-[500px] break-words mb-6 rounded-md px-5 py-5 mx-auto xl:ml-20 lg:ml-16 sm:w-[440px] md:w-[720px] lg:w-[820px] xl:w-[1100px]">
         <TrondheimTab open={openTab === 1} />
         <AasTab open={openTab === 2} />
         <BergenTab open={openTab === 3} />
