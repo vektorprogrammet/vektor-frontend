157b08e1fef8c58849abee9f2e45281b9136a52c
phibkro <71797726+phibkro@users.noreply.github.com>
2025-02-10 19:31:08 +0100

    chore: :heavy_plus_sign: add react-router deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 /// <reference types="vitest" />
-import react from "@vitejs/plugin-react";
+import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactRouter()],
   build: {
     outDir: "./build",
   },
