592352d7dcc400af4c155812a25b6060a9882bc8
phibkro <71797726+phibkro@users.noreply.github.com>
2025-03-20 12:37:29 +0100

    chore: :heavy_minus_sign: remove unused deps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
   darkMode: "class", // or false or 'media'
   theme: {
