3beed8119e7a164dec240777654db8d233b8dcaa
phibkro <71797726+phibkro@users.noreply.github.com>
2025-03-21 01:05:14 +0100

    refactor: :art: pass components as props instead of nesting

--- src/routes/_home.kontakt.tsx
+++ src/routes/_home.kontakt.tsx
@@ -1,6 +1,7 @@
-import { getKontakt } from "~/api/kontakt";
 import { Label } from "@radix-ui/react-label";
+import type { JSX } from "react";
 import { useEffect, useState } from "react";
+import { getKontakt } from "~/api/kontakt";
 import { Tabs } from "~/components/tabs";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
@@ -30,11 +31,23 @@ export default function Kontakt() {
         {kontaktInfo.title}
       </h1>
       <KontaktTabs
-        divisions={[
-          { name: "Trondheim", number: 1 },
-          { name: "Ås", number: 2 },
-          { name: "Bergen", number: 3 },
-          { name: "Hovedstyret", number: 4 },
+        departments={[
+          {
+            name: "Trondheim",
+            comp: TrondheimTab,
+          },
+          {
+            name: "Ås",
+            comp: AasTab,
+          },
+          {
+            name: "Bergen",
+            comp: BergenTab,
+          },
+          {
+            name: "Hovedstyret",
+            comp: HovedstyretTab,
+          },
         ]}
       />
     </div>
@@ -43,155 +56,142 @@ export default function Kontakt() {
 
 /* Tabs */
 
-interface KontaktTabProps {
-  divisions: Array<DivisionList>;
+function TrondheimTab() {
+  return (
+    <Division
+      name="Trondheim"
+      subtitle="Norges teknisk-naturvitenskapelige universitet"
+      mail="styret.ntnu@vektorprogrammet.no"
+      address="Høgskoleringen 5, 7491 Trondheim"
+      contactInfos={[
+        { name: "Styret", mail: "styret.ntnu@vektorprogrammet.no" },
+        { name: "Evaluering", mail: "evaluering.ntnu@vektorprogrammet.no" },
+        {
+          name: "Rekruttering",
+          mail: "rekruttering.ntnu@vektorprogrammet.no",
+        },
+        {
+          name: "Skolekoordinering",
+          mail: "skolekoordinering.ntnu@vektorprogrammet.no",
+        },
+        { name: "Sponsor", mail: "sponsor.ntnu@vektorprogrammet.no" },
+        { name: "Økonomi", mail: "okonomi@vektorprogrammet.no" },
+        { name: "IT", mail: "it@vektorprogrammet.no" },
+        { name: "Profilering", mail: "profilering.ntnu@vektorprogrammet.no" },
+      ]}
+      contact
+    />
+  );
 }
 
-interface DivisionList {
-  name: string;
-  number: number;
+function AasTab() {
+  return (
+    <Division
+      name="Ås"
+      subtitle="Norges miljø- og biovitenskapelige universitet"
+      mail="nmbu@vektorprogrammet.no"
+      contactInfos={[
+        { name: "Styret", mail: "nmbu@vektorprogrammet.no" },
+        {
+          name: "Sponsor og økonomi",
+          mail: "sponsor.nmbu@vektorprogrammet.no",
+        },
+        {
+          name: "Skolekoordinering",
+          mail: "skolekoordinering.nmbu@vektorprogrammet.no",
+        },
+        {
+          name: "Evaluering, Rekruttering og Profilering",
+          mail: "evaluering.nmbu@vektorprogrammet.no",
+        },
+      ]}
+      contact
+    />
+  );
 }
 
-const TrondheimTab = ({ open }: { open: boolean }) => {
+function BergenTab() {
   return (
-    <div className={`flex-grow ${open ? "block" : "hidden"}`}>
-      <Division
-        name="Trondheim"
-        subtitle="Norges teknisk-naturvitenskapelige universitet"
-        mail="styret.ntnu@vektorprogrammet.no"
-        address="Høgskoleringen 5, 7491 Trondheim"
-        contactInfos={[
-          { name: "Styret", mail: "styret.ntnu@vektorprogrammet.no" },
-          { name: "Evaluering", mail: "evaluering.ntnu@vektorprogrammet.no" },
-          {
-            name: "Rekruttering",
-            mail: "rekruttering.ntnu@vektorprogrammet.no",
-          },
-          {
-            name: "Skolekoordinering",
-            mail: "skolekoordinering.ntnu@vektorprogrammet.no",
-          },
-          { name: "Sponsor", mail: "sponsor.ntnu@vektorprogrammet.no" },
-          { name: "Økonomi", mail: "okonomi@vektorprogrammet.no" },
-          { name: "IT", mail: "it@vektorprogrammet.no" },
-          { name: "Profilering", mail: "profilering.ntnu@vektorprogrammet.no" },
-        ]}
-        contact
-      />
-    </div>
+    <Division
+      name="Bergen"
+      subtitle="Universitetet i Bergen"
+      mail="uib@vektorprogrammet.no"
+      contactInfos={[
+        { name: "Styret", mail: "uib@vektorprogrammet.no" },
+        {
+          name: "Skolekoordinering",
+          mail: "skolekoordinering.uib@vektorprogrammet.no",
+        },
+        {
+          name: "Rekruttering",
+          mail: "rekruttering.uib@vektorprogrammet.no",
+        },
+      ]}
+      contact
+    />
   );
-};
+}
 
-const AasTab = ({ open }: { open: boolean }) => {
+function HovedstyretTab() {
   return (
-    <div className={`flex-grow ${open ? "block" : "hidden"}`}>
-      <Division
-        name="Ås"
-        subtitle="Norges miljø- og biovitenskapelige universitet"
-        mail="nmbu@vektorprogrammet.no"
-        contactInfos={[
-          { name: "Styret", mail: "nmbu@vektorprogrammet.no" },
-          {
-            name: "Sponsor og økonomi",
-            mail: "sponsor.nmbu@vektorprogrammet.no",
-          },
-          {
-            name: "Skolekoordinering",
-            mail: "skolekoordinering.nmbu@vektorprogrammet.no",
-          },
-          {
-            name: "Evaluering, Rekruttering og Profilering",
-            mail: "evaluering.nmbu@vektorprogrammet.no",
-          },
-        ]}
-        contact
-      />
-    </div>
+    <Division
+      name="Hovedstyret"
+      description="Hovedstyret er det nasjonale styret i vektorprogrammet. De er et overordnet organ med ansvar for drifting av hele organisasjonen."
+      mail="hovedstyret@vektorprogrammet.no"
+      members={8}
+      button
+      contactInfos={[
+        {
+          name: "Inga Bordal",
+          title: "Leder",
+          mail: "inga.bordal@vektorprogrammet.no",
+        },
+        {
+          name: "Emma Dyvesveen Myrbekk",
+          title: "Nestleder",
+          mail: "emma.dyvesveen@vektorprogrammet.no",
+        },
+        {
+          name: "Andreas Hope Pedersen",
+          title: "Ekspansjon",
+          mail: "andreas.pedersen@vektorprogrammet.no",
+        },
+        {
+          name: "Erlend Marius Ommundsen",
+          title: "IT-ansvarlig",
+          mail: "erlend.marius@vektorprogrammet.no",
+        },
+        {
+          name: "David Ramsvik",
+          title: "Mentor",
+          mail: "david@vektorprogrammet.no",
+        },
+        {
+          name: "Ingeborg Eldevik Rusaas",
+          title: "Profilering",
+          mail: "ingeborg.eldevik@vektorprogrammet.no",
+        },
+        {
+          name: "Adrian Larsen",
+          title: "Sponsor",
+          mail: "adrian@vektorprogrammet.no",
+        },
+        {
+          name: "Odin Nilsen",
+          title: "Økonomi",
+          mail: "odin@vektorprogrammet.no",
+        },
+      ]}
+      contact={false}
+    />
   );
-};
+}
 
-const BergenTab = ({ open }: { open: boolean }) => {
-  return (
-    <div className={`flex-grow ${open ? "block" : "hidden"}`}>
-      <Division
-        name="Bergen"
-        subtitle="Universitetet i Bergen"
-        mail="uib@vektorprogrammet.no"
-        contactInfos={[
-          { name: "Styret", mail: "uib@vektorprogrammet.no" },
-          {
-            name: "Skolekoordinering",
-            mail: "skolekoordinering.uib@vektorprogrammet.no",
-          },
-          {
-            name: "Rekruttering",
-            mail: "rekruttering.uib@vektorprogrammet.no",
-          },
-        ]}
-        contact
-      />
-    </div>
-  );
-};
-
-const HovedstyretTab = ({ open }: { open: boolean }) => {
-  return (
-    <div className={`flex-grow ${open ? "block" : "hidden"}`}>
-      <Division
-        name="Hovedstyret"
-        description="Hovedstyret er det nasjonale styret i vektorprogrammet. De er et overordnet organ med ansvar for drifting av hele organisasjonen."
-        mail="hovedstyret@vektorprogrammet.no"
-        members={8}
-        button
-        contactInfos={[
-          {
-            name: "Inga Bordal",
-            title: "Leder",
-            mail: "inga.bordal@vektorprogrammet.no",
-          },
-          {
-            name: "Emma Dyvesveen Myrbekk",
-            title: "Nestleder",
-            mail: "emma.dyvesveen@vektorprogrammet.no",
-          },
-          {
-            name: "Andreas Hope Pedersen",
-            title: "Ekspansjon",
-            mail: "andreas.pedersen@vektorprogrammet.no",
-          },
-          {
-            name: "Erlend Marius Ommundsen",
-            title: "IT-ansvarlig",
-            mail: "erlend.marius@vektorprogrammet.no",
-          },
-          {
-            name: "David Ramsvik",
-            title: "Mentor",
-            mail: "david@vektorprogrammet.no",
-          },
-          {
-            name: "Ingeborg Eldevik Rusaas",
-            title: "Profilering",
-            mail: "ingeborg.eldevik@vektorprogrammet.no",
-          },
-          {
-            name: "Adrian Larsen",
-            title: "Sponsor",
-            mail: "adrian@vektorprogrammet.no",
-          },
-          {
-            name: "Odin Nilsen",
-            title: "Økonomi",
-            mail: "odin@vektorprogrammet.no",
-          },
-        ]}
-        contact={false}
-      />
-    </div>
-  );
-};
-
-const KontaktTabs = ({ divisions }: KontaktTabProps) => {
+function KontaktTabs({
+  departments,
+}: {
+  departments: Array<{ name: string; comp: () => JSX.Element }>;
+}) {
   const initialTabState = () => {
     const storedTab = sessionStorage.getItem("kontaktTab");
     return storedTab ? Number.parseInt(storedTab, 10) : 1;
@@ -200,24 +200,26 @@ const KontaktTabs = ({ divisions }: KontaktTabProps) => {
   useEffect(() => {
     sessionStorage.setItem("kontaktTab", openTab.toString());
   }, [openTab]);
+
+  const Tab = departments[openTab].comp;
+  const names = departments.map((dep) => dep.name);
+  const keys = names.map((name, i) => {
+    return { name: name, number: i };
+  });
+
   return (
     <div className="mb-6 flex flex-col items-start md:mb-auto md:max-w-6xl md:flex-row">
       <div className="ml-3 w-1/5">
-        <Tabs
-          divisions={divisions}
-          tabstate={openTab}
-          setOpenTab={setOpenTab}
-        />
+        <Tabs divisions={keys} tabstate={openTab} setOpenTab={setOpenTab} />
       </div>
       <div className="mx-auto mb-6 flex h-[500px] w-full flex-col items-start overflow-y-scroll break-words rounded-md px-5 py-5 sm:w-[440px] md:w-[720px] lg:ml-16 lg:w-[820px] xl:ml-20 xl:w-[1100px]">
-        <TrondheimTab open={openTab === 1} />
-        <AasTab open={openTab === 2} />
-        <BergenTab open={openTab === 3} />
-        <HovedstyretTab open={openTab === 4} />
+        <div className="flex-grow">
+          <Tab />
+        </div>
       </div>
     </div>
   );
-};
+}
 
 /* Division */
 
