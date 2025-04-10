9b20cdf99a6ff778ed05b213e4259ccc904560e6
Helene Selvig <helenekselvig@gmail.com>
2025-04-10 10:44:33 +0200

    feat: add faq to team page

--- src/routes/_home.team.tsx
+++ src/routes/_home.team.tsx
@@ -1,11 +1,21 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { getTeamFaqs } from "~/api/faq";
+import { Divider } from "~/components/divider";
+
 import { Outlet } from "react-router";
 import { getTeam } from "~/api/team";
 
 // biome-ignore lint/style/noDefaultExport: Route Modules require default export https://reactrouter.com/start/framework/route-module
 export default function Team() {
   const teamInfo = getTeam();
+  const teamFaqs = getTeamFaqs();
   return (
-    <div className="mx-auto mt-10 mb-20 flex w-full max-w-6xl flex-col items-center">
+    <div className="mx-auto mt-20 mb-20 flex w-full max-w-6xl flex-col items-center">
       <header className="mx-auto flex w-full flex-wrap justify-around">
         <div className="mt-5 flex max-w-6xl flex-col">
           <h2 className="mx-3 font-bold text-4xl text-gray-600 dark:text-gray-200">
@@ -42,6 +52,28 @@ export default function Team() {
         {teamInfo.title}
       </h1>
       <Outlet />
+      <Divider />
+
+      {/* FAQ Section */}
+      <div className="flex w-4/5 max-w-4xl flex-col items-center gap-10 self-center md:mt-20 dark:text-text-dark">
+        <h2 className="w-full text-center font-bold text-2xl text-vektor-DARKblue md:text-4xl dark:text-text-dark">
+          {"Ofte stilte spørsmål"}
+        </h2>
+        <div className="flex w-full flex-col items-center">
+          <Accordion type="single" collapsible className="w-full">
+            {teamFaqs.map(({ question, answer }) => (
+              <AccordionItem key={question} value={question}>
+                <AccordionTrigger>
+                  <p className="text-left">{question}</p>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <p className="text-left">{answer}</p>
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
+      </div>
     </div>
   );
 }
